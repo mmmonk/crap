381c42c6f835b55578055c0cc8618b55cdcd9973
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2008-05-26 11:30:22 +0000

    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@9 aaeee498-0047-0410-bd38-07bdd6096719

--- socks4a_cli.py
+++ socks4a_cli.py
@@ -7,16 +7,18 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 1080))
 
 host = "212.77.100.101"
+port = 80
+ver  = 4
 
 host = socket.inet_aton(socket.gethostbyname(host))
-data = struct.pack('!BBH',4,1,80)+host+chr(0)
+data = struct.pack('!BBH',ver,1,port)+host+chr(0)
 
 s.send(data);
 
 data = s.recv(1024)
 
 ver,code,head = struct.unpack('BBH',data[:4])
-print ' %d %d %d ' % (ver, code, head)
+print ' %s %d %s ' % (ver, code, head)
 
 s.close()
 

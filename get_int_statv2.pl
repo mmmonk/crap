b2b42ea5bbfc7687f955b1b0dc4191f33934dade
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2008-06-03 12:18:57 +0000

    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@15 aaeee498-0047-0410-bd38-07bdd6096719

--- get_int_statv2.pl
+++ get_int_statv2.pl
@@ -5,11 +5,14 @@
 #
 
 use strict;
+use integer;
+use warnings;
 
 my $file=shift;
 
 my $last=0;
 my $sec=0;
+my $tdiff=0;
 my $if;
 my $inside_tfc=0;
 my %if_stat;
@@ -33,30 +36,41 @@ while(<FD>){
 		$last=$sec;	
 		($sec=$_)=~s/.*?(\d+?)\..*/$1/;
 
+		$tdiff=$sec-$last;
+
 		my $totalinpps=0;
 		my $totalinbps=0;
+		my $totalinvpn=0;
+
 		if ($last ne 0){
 			foreach my $stat (%if_stat){
 				$totalinpps+=$if_stat{$stat} if ($stat=~/inpackets/);
 				$totalinbps+=$if_stat{$stat} if ($stat=~/inbytes/);
+				$totalinvpn+=$if_stat{$stat} if ($stat=~/invpn/);
 			}
-
 			
 			unless ($if_stat{"total-inpps"}==0){
-				print "All interfaces in pps =\t",int(($totalinpps-$if_stat{"total-inpps"})/($sec-$last)),"\n";
+				print "All interfaces in pps =\t",int(($totalinpps-$if_stat{"total-inpps"})/($tdiff)),"\n";
 				$tot{"inpps"}+=($totalinpps-$if_stat{"total-inpps"});
 			}
 
-#			unless ($if_stat{"total-inbps"}==0){
-#				print "All interfaces in bps =\t",int(($totalinbps-$if_stat{"total-inbps"})/($sec-$last)),"\n\n";
-#				$tot{"inbps"}+=($totalinbps-$if_stat{"total-inbps"});
-#			}
+			unless ($if_stat{"total-inbps"}==0){
+				print "All interfaces in bps =\t",int(($totalinbps-$if_stat{"total-inbps"})/($tdiff)),"\n";
+				$tot{"inbps"}+=($totalinbps-$if_stat{"total-inbps"});
+			}
 
-			print "For this calculation the time slot was: ",($sec-$last)," seconds\n";
-			$tot{"sec"}+=($sec-$last);
+			unless ($if_stat{"total-invpn"}==0){
+				print "All interfaces in vpn =\t",int(($totalinvpn-$if_stat{"total-invpn"})/($tdiff)),"\n";
+				$tot{"invpn"}+=($totalinvpn-$if_stat{"total-invpn"});
+			}
+
+
+			print "\nFor this calculation the time slot was: ",($tdiff)," seconds\n";
+			$tot{"sec"}+=($tdiff);
 		}
 		$if_stat{"total-inpps"}=$totalinpps;
 		$if_stat{"total-inbps"}=$totalinbps;
+		$if_stat{"total-invpn"}=$totalinvpn;
 	}
 	if (/counters for interface/){
                 $inside_tfc=0;
@@ -66,14 +80,14 @@ while(<FD>){
 		$inside_tfc=1;
 	}
 
-	if ($inside_tfc==1 and /^in (packets|bytes)/){
+	if ($inside_tfc==1 and /^in (packets|bytes|vpn)/){
 		my $temp;
 		if (/ packets /){
 			($temp=$_)=~s/in packets\s+(\d+) \|.*/$1/;
 			if (exists($if_stat{"$if-inpackets"})){
-				my $pps=int(($temp-$if_stat{"$if-inpackets"})/($sec-$last));
-				if ($pps>0){
-					print "$if in pps =\t".$pps."\n";
+				my $temp1=int(($temp-$if_stat{"$if-inpackets"})/($tdiff));
+				if ($temp1>0){
+					print "$if in pps =\t".$temp1."\n";
 				}
 			}
 			$if_stat{"$if-inpackets"}=$temp;
@@ -81,12 +95,22 @@ while(<FD>){
 		if (/ bytes /){
 			($temp=$_)=~s/in bytes\s+(\d+) \|.*/$1/;
 			if (exists($if_stat{"$if-inbytes"})){
-				my $bps=int(($temp-$if_stat{"$if-inbytes"})/($sec-$last));
-				if ($bps>0){
-#					print "$if in bps =\t".$bps."\n";
+				my $temp1=int(($temp-$if_stat{"$if-inbytes"})/($tdiff));
+				if ($temp1>0){
+					print "$if in bps =\t".$temp1."\n";
 				}
 			}
 			$if_stat{"$if-inbytes"}=$temp;
+		}
+		if (/ vpn /){
+			($temp=$_)=~s/in vpn\s+(\d+) \|.*/$1/;
+			if (exists($if_stat{"$if-invpn"})){
+				my $temp1=int(($temp-$if_stat{"$if-invpn"})/($tdiff));
+				if ($temp1>0){
+					print "$if in vpn =\t".$temp1."\n";
+				}
+			}
+			$if_stat{"$if-invpn"}=$temp;
 		}
 	}
 }
@@ -96,21 +120,26 @@ close(FD);
 if ($clock_check>1){
 	my $totalinpps=0;
 	my $totalinbps=0;
+	my $totalinvpn=0;
 	foreach my $stat (%if_stat){
 		$totalinpps+=$if_stat{$stat} if ($stat=~/inpackets/);
 		$totalinbps+=$if_stat{$stat} if ($stat=~/inbytes/);
+		$totalinvpn+=$if_stat{$stat} if ($stat=~/invpn/);
 	}
 
 	$tot{"inpps"}+=($totalinpps-$if_stat{"total-inpps"});
 	$tot{"inbps"}+=($totalinbps-$if_stat{"total-inbps"});
+	$tot{"invpn"}+=($totalinvpn-$if_stat{"total-invpn"});
 
-	print "All interfaces in pps =\t",int(($totalinpps-$if_stat{"total-inpps"})/($sec-$last)),"\n";
-	#print "All interfaces in bps =\t",int(($totalinbps-$if_stat{"total-inbps"})/($sec-$last)),"\n\n";
+	print "All interfaces in pps =\t",int(($totalinpps-$if_stat{"total-inpps"})/($tdiff)),"\n"   unless ($if_stat{"total-inpps"}==0);
+	print "All interfaces in bps =\t",int(($totalinbps-$if_stat{"total-inbps"})/($tdiff)),"\n"   unless ($if_stat{"total-inbps"}==0);
+	print "All interfaces in vpn =\t",int(($totalinvpn-$if_stat{"total-invpn"})/($tdiff)),"\n\n" unless ($if_stat{"total-invpn"}==0);
 
-	print "Total time of the file = ",$tot{"sec"}," seconds\n";
+	print "\n-------------------------------\nTotal time of the file = ",$tot{"sec"}," seconds\n";
 	print "Avg in pps             = ",int($tot{"inpps"}/$tot{"sec"}),"\n";
-	#print "Avg in bps             = ",int($tot{"inbps"}/$tot{"sec"}),"\n";
-	#print "Avg pkt size           = ",int($tot{"inbps"}/$tot{"inpps"}),"\n";
+	print "Avg in bps             = ",int($tot{"inbps"}/$tot{"sec"}),"\n";
+	print "Avg in vpn             = ",int($tot{"invpn"}/$tot{"sec"}),"\n";
+	print "Avg pkt size           = ",int($tot{"inbps"}/$tot{"inpps"}),"\n";
 }else{
 	print "To few \"get clock\"s found\n";
 }

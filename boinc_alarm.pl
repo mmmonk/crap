2dfa68c0afb2bf5fd2d7b9be25a7a45362269d36
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2008-07-04 14:31:39 +0000

    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@18 aaeee498-0047-0410-bd38-07bdd6096719

---
+++ boinc_alarm.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @temp=`/usr/bin/sensors | /bin/grep Core`;
+
+my $maxtemp=0;
+foreach (@temp){
+	s/^.+?(\d+)\.\d+.*/$1/;
+	$maxtemp=$_ if ($_>$maxtemp);
+}
+
+open(FD,"/etc/boinc-client/global_prefs_override.xml");
+my @cfg=<FD>;
+close(FD);
+
+my $cpulimit=0;
+
+foreach (@cfg){
+	if (/cpu_usage_limit/){
+		chomp;	
+		($cpulimit=$_)=~s/\s+<cpu_usage_limit>(\d+)\.000000<\/cpu_usage_limit>/$1/;
+		if ($maxtemp<70){
+			$cpulimit+=5;
+			$cpulimit=100 if ($cpulimit>100);
+		}else{	
+			$cpulimit-=5;
+			$cpulimit=10 if ($cpulimit<10);
+		}
+		s/<cpu_usage_limit>\d+\.000000<\/cpu_usage_limit>/<cpu_usage_limit>$cpulimit\.000000<\/cpu_usage_limit>\n/;
+	}
+}
+
+open(FD,"> /etc/boinc-client/global_prefs_override.xml");
+print FD @cfg;
+close(FD);
+exec("/usr/bin/boinc_cmd --host 127.0.0.1 --passwd \"\" --read_global_prefs_override");
---
+++ boinc_set_cpu.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $cpulimit=shift;
+
+die ":P" unless ($cpulimit);
+
+die ":P" if ($cpulimit>100 or $cpulimit<0); 
+
+open(FD,"/etc/boinc-client/global_prefs_override.xml");
+my @cfg=<FD>;
+close(FD);
+
+foreach (@cfg){
+	s/<cpu_usage_limit>\d+\.000000<\/cpu_usage_limit>/<cpu_usage_limit>$cpulimit\.000000<\/cpu_usage_limit>/ if (/cpu_usage_limit/);
+}
+
+open(FD,"> /etc/boinc-client/global_prefs_override.xml");
+print FD @cfg;
+close(FD);
+exec("/usr/bin/boinc_cmd --host 127.0.0.1 --passwd \"\" --read_global_prefs_override");

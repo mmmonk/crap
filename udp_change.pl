7699fc5931cf33d1d3bc79ad09474b3fb7057305
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2008-08-04 13:16:24 +0000

    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@22 aaeee498-0047-0410-bd38-07bdd6096719

--- udp_change.pl
+++ udp_change.pl
@@ -1,10 +1,13 @@
 #!/usr/bin/perl
 
+#
+# author m.lukaszuk@gmail.com
+#
+
 use strict;
 use warnings;
 use integer;
-use IO::Socket;
-use POSIX;
+use IO::Socket::INET;
 
 my $lport=shift;
 my $dport=shift;
@@ -18,22 +21,22 @@ my $srv = IO::Socket::INET->new(LocalAddr => '172.30.73.133', Proto => "udp", Lo
 my $cli = IO::Socket::INET->new(Proto => "udp", PeerPort => $dport, PeerAddr => "127.0.0.1") or die "Couldn't create socket: $!\n";
 
 while ($srv->recv($data, $MAX_TO_READ)) {
-	print "######### new request ############\n";
-	print ">  ".time." got something\n";
-	print ">  ".time." sending it the server\n";
-	$cli->send($data);
-	print "<  ".time." got answer\n";
-	$cli->recv($data,$MAX_TO_READ);
+	if (fork()==0){
+		print "$$ ######### new request ############\n";
+		print "$$ >  ".time." sending it the server\n";
+		$cli->send($data);
+		print "$$ <  ".time." got answer\n";
+		$cli->recv($data,$MAX_TO_READ);
 
-	my $dat1=unpack('H*',$data);
-	print $dat1,"\n";
-	my $TTL1=rand(10000000)+30000000;
-	my $TTL2=sprintf("%8x",$TTL1);
-	$TTL2=~s/\s/0/g;
-	$dat1=~s/c00c00010001.{8}/c00c00010001$TTL2/g;
-	print $dat1,"\n";
-	my $dat2=pack('H*',$dat1);
-	print unpack('H*',$dat2),"\n";
-	print ">  ".time." sending to the client\n";
-	$srv->send($dat2);
-} 
+		my $dat1=unpack('H*',$data);
+		my $TTL1=rand(10000000)+30000000;
+		my $TTL2=sprintf("%8x",$TTL1);
+		$TTL2=~s/\s/0/g;
+		$dat1=~s/c00c00010001.{8}/c00c00010001$TTL2/g;
+		my $dat2=pack('H*',$dat1);
+		print "$$ >  ".time." sending to the client\n";
+		$srv->send($dat2);
+		exit;
+	}
+}
+

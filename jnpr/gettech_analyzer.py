91d8808b0bb12cee90294966f20ec8c19e124caf
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2009-06-25 09:43:16 +0000

    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@34 aaeee498-0047-0410-bd38-07bdd6096719

--- jnpr/gettech_analyzer.py
+++ jnpr/gettech_analyzer.py
@@ -1,7 +1,61 @@
 #!/usr/bin/env python
 
-fs = open('get-tech-support-10102008.txt','r')
+import sys
+
+GetCommands = [
+	'get admin auth ',
+	'get arp ',
+	'get asp ',
+	'get auth ',
+	'get av ',
+	'get cav ',
+	'get chassis ',
+	'get config ',
+	'get counter ',
+	'get dns ',
+	'get envar ',
+	'get file ',
+	'get fresno ',
+	'get gbic ',
+	'get ha ',
+	'get imp ',
+	'get iocard ',
+	'get jupiter ',
+	'get license-key ',
+	'get mac-learn ',
+	'get memory ',
+	'get net-pak ',
+	'get nvram ',
+	'get os ',
+	'get performance ',
+	'get pim ',
+	'get pki ldap-run ',
+	'get route ',
+	'get sat ',
+	'get session ',
+	'get system ',
+	'get tcp ',
+	'get vpnmonitor ',
+	'get vrouter protocol bgp ',
+	'get vrouter protocol nhrp ',
+	'get vrouter protocol ospf ',
+	'get vrouter protocol pim ',
+	'get vrouter protocol rip '
+]
+
+
+try:
+	fs = open(sys.argv[1],'r')
+except:
+	print "usage: "+sys.argv[0]+" get_tech_file"
+	sys.exit(2)
+
 
 for line in fs.readlines():
 	if 'get ' in line:
-		print line
+		FuncCount = 0
+		for getline in GetCommands:
+			if line in getline:
+				break
+			FuncCount=FuncCount+1
+		print FuncCount

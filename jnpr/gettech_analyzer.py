2d20d60384b1188e6eb0f5be96dfc01a29681f26
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2009-06-26 05:12:24 +0000

    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@40 aaeee498-0047-0410-bd38-07bdd6096719

--- jnpr/gettech_analyzer.py
+++ jnpr/gettech_analyzer.py
@@ -3,44 +3,44 @@
 import sys
 
 GetCommands = [
-	'get admin auth ',
-	'get arp ',
-	'get asp ',
-	'get auth ',
-	'get av ',
-	'get cav ',
-	'get chassis ',
-	'get config ',
-	'get counter ',
-	'get dns ',
-	'get envar ',
-	'get file ',
-	'get fresno ',
-	'get gbic ',
-	'get ha ',
-	'get imp ',
-	'get iocard ',
-	'get jupiter ',
-	'get license-key ',
-	'get mac-learn ',
-	'get memory ',
-	'get net-pak ',
-	'get nvram ',
-	'get os ',
-	'get performance ',
-	'get pim ',
-	'get pki ldap-run ',
-	'get route ',
-	'get sat ',
-	'get session ',
-	'get system ',
-	'get tcp ',
-	'get vpnmonitor ',
-	'get vrouter protocol bgp ',
-	'get vrouter protocol nhrp ',
-	'get vrouter protocol ospf ',
-	'get vrouter protocol pim ',
-	'get vrouter protocol rip '
+	'get admin auth',
+	'get arp',
+	'get asp',
+	'get auth',
+	'get av',
+	'get cav',
+	'get chassis',
+	'get config',
+	'get counter',
+	'get dns',
+	'get envar',
+	'get file',
+	'get fresno',
+	'get gbic',
+	'get ha',
+	'get imp',
+	'get iocard',
+	'get jupiter',
+	'get license-key',
+	'get mac-learn',
+	'get memory',
+	'get net-pak',
+	'get nvram',
+	'get os',
+	'get performance',
+	'get pim',
+	'get pki ldap-run',
+	'get route',
+	'get sat',
+	'get session',
+	'get system',
+	'get tcp',
+	'get vpnmonitor',
+	'get vrouter protocol bgp',
+	'get vrouter protocol nhrp',
+	'get vrouter protocol ospf',
+	'get vrouter protocol pim',
+	'get vrouter protocol rip'
 ]
 
 
@@ -55,7 +55,7 @@ for line in fs.readlines():
 	if 'get ' in line:
 		FuncCount = 0
 		for getline in GetCommands:
-			if line in getline:
+			if getline in line:
 				break
 			FuncCount=FuncCount+1
 		print FuncCount

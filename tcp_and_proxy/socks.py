1027b59df295568f5ee2b519ab04e51c395a1553
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2009-12-12 07:28:21 +0000

    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@60 aaeee498-0047-0410-bd38-07bdd6096719

--- tcp_and_proxy/socks.py
+++ tcp_and_proxy/socks.py
@@ -1,37 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/python -u
 
+import os
 import socket
 import struct
 import sys
-from threading import Thread
+import select
+import fcntl
 
-
-def socks_read(socks):
-  
-  while 1:
-    data = socks.recv(1500)
-    if data:
-      sys.stdout.write(data)
-    else:
-      break
-
-def socks_write(socks):
-
-  while 1:
-    data = sys.stdin.read()
-    if data:
-      socks.send(data)
-    else:
-      break
+phost = sys.argv[1]
+pport = int(sys.argv[2])
+host  = sys.argv[3]
+port  = int(sys.argv[4])
+ver   = 5
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 1080))
-
-host = sys.argv[1]
-port = int(sys.argv[2])
-ver  = 5
-
-exit = 0
+s.connect((phost, pport))
 
 if ver == 5:
   data = struct.pack('!3B',5,1,0)
@@ -45,7 +28,7 @@ if ver == 5:
     except socket.error:
       data = struct.pack('!5B',5,1,0,3,len(host))+host+nport
   else:
-      exit = 1
+    sys.exit()
 
 elif ver == 4 or ver == '4a':
   try:
@@ -54,20 +37,27 @@ elif ver == 4 or ver == '4a':
     data = struct.pack('!2BH',4,1,port)+socket.inet_aton('0.0.0.1')+chr(0)+host+chr(0)
 
 else:
-  exit = 1
+  sys.exit()
 
-if exit != 1:
-  s.send(data)
-  data = s.recv(256)
-  code = struct.unpack('BBH',data[:4])[1]
-  
-  if (code == 90 and ver == 4) or (code == 0 and ver == 5): 
-    sread = Thread(target=socks_read, args=(s,))
-    swrite = Thread(target=socks_write, args=(s,))
+s.send(data)
+data = s.recv(256)
+code = struct.unpack('BBH',data[:4])[1]
 
-    sread.start()
-    swrite.start()
-    sread.join()
-    swrite.join()
+if (code == 90 and ver == 4) or (code == 0 and ver == 5): 
+
+  fcntl.fcntl(s, fcntl.F_SETFL, os.O_NONBLOCK|os.O_NDELAY) 
+  fcntl.fcntl(0, fcntl.F_SETFL, os.O_NONBLOCK)
+
+  while 1:
+    toread,[],[]=select.select([sys.stdin,s],[],[],30)
+    
+    if s in toread:
+      data = s.recv(1500)
+      if data:
+          sys.stdout.write(data)
+    if sys.stdin in toread:
+      data = sys.stdin.read(1500)
+      if data:
+          s.send(data)
 
 s.close()

76da963c5af4d0f88bef9ccc6c34baaad7296637
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2009-12-15 16:43:00 +0000

    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@65 aaeee498-0047-0410-bd38-07bdd6096719

--- jnpr/gettech_analyzer.py
+++ jnpr/gettech_analyzer.py
@@ -44,21 +44,69 @@ GetCommands = [
 ]
 
 
+FuncCommands = [
+	'get_admin_auth',
+	'get_arp',
+	'get_asp',
+	'get_auth',
+	'get_av',
+	'get_cav',
+	'get_chassis',
+	'get_config',
+	'get_counter',
+	'get_dns',
+	'get_envar',
+	'get_file',
+	'get_fresno',
+	'get_gbic',
+	'get_ha',
+	'get_imp',
+	'get_iocard',
+	'get_jupiter',
+	'get_license-key',
+	'get_mac-learn',
+	'get_memory',
+	'get_net-pak',
+	'get_nvram',
+	'get_os',
+	'get_performance',
+	'get_pim',
+	'get_pki_ldap-run',
+	'get_route',
+	'get_sat',
+	'get_session',
+	'get_system',
+	'get_tcp',
+	'get_vpnmonitor',
+	'get_vrouter_protocol_bgp',
+	'get_vrouter_protocol_nhrp',
+	'get_vrouter_protocol_ospf',
+	'get_vrouter_protocol_pim',
+	'get_vrouter_protocol_rip'
+]
+
+def get_envar(input):
+    print input
+    
+
 try:
 	fs = open(sys.argv[1],'r')
 except:
 	print "usage: "+sys.argv[0]+" get_tech_file"
 	sys.exit(2)
 
-def get_sat():
-  
-
+FuncCount = 0
 for line in fs.readlines():
+    if FuncCount > 0:
+        fname = FuncCommands[FuncCount]
+        print fname
+        f = locals()[fname]
+        f() 
+
 	if 'get ' in line:
 		FuncCount = 0
 		for getline in GetCommands:
 			if getline in line:
 				break
 			FuncCount=FuncCount+1
-		print FuncCount
-
+        print str(FuncCount)+" "+line
---
+++ pcap/pcap_raw.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import pcap
+import sys
+import string
+import struct
+from socket import *
+
+proto = 0x55aa
+
+s = socket(AF_PACKET, SOCK_RAW, proto)
+s.bind(("wlan0",proto))
+
+ifName,ifProto,pktType,hwType,hwAddr = s.getsockname()
+
+srcAddr = hwAddr
+dstAddr = "\x01\x02\x03\x04\x05\x06"
+ethData = "here is some data for an ethernet packet"
+
+txFrame = struct.pack("!6s6sh",dstAddr,srcAddr,proto) + ethData
+
+print "Tx[%d]: "%len(ethData) + string.join(["%02x"%ord(b) for b in
+ethData]," ")
+
+s.send(txFrame)
+
+rxFrame = s.recv(2048)
+
+dstAddr,srcAddr,proto = struct.unpack("!6s6sh",rxFrame[:14])
+ethData = rxFrame[14:]
+
+print "Rx[%d]: "%len(ethData) + string.join(["%02x"%ord(b) for b in
+ethData]," ")
+
+s.close()
+

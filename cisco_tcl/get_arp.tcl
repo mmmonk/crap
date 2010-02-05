6cb6eeccac051f7760c71a7b5888aac56c24a70c
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2010-02-05 16:11:00 +0000

    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@86 aaeee498-0047-0410-bd38-07bdd6096719

---
+++ cisco_tcl/get_arp.tcl
@@ -0,0 +1,12 @@
+proc dhcpmnk {} {
+  set log [open "syslog:" w+]
+  set arpall [exec "show arp"]
+  foreach line [regexp -all -line -inline "^Internet .* Vlan1" $arpall] {
+    set tokens [regexp -inline -all -- {\S+} $line] 
+    set ip [lindex $tokens 1]
+    set mac [lindex $tokens 3]
+    set txt [concat "%DHCP-7-MONKEY:" $ip " " $mac] 
+    puts $log $txt
+  }
+  close $log
+}

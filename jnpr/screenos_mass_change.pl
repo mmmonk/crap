92e181821e82de5d98694acfff96d74e7fcda983
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2010-09-05 12:28:13 +0000

    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@160 aaeee498-0047-0410-bd38-07bdd6096719

--- jnpr/screenos_mass_change.pl
+++ jnpr/screenos_mass_change.pl
@@ -9,8 +9,9 @@ use integer;
 use Expect;
 use POSIX qw(:termios_h);
 
-# max children running
+# max children running around
 my $maxkids=64;
+
 # timeout for the spawn in expect process
 my $timeout=30;
 
@@ -20,6 +21,10 @@ my $password="netscreen";
 my $hostsfile=shift;
 my $cmdsfile=shift;
 my @cmds;
+
+sub timer;
+
+my $starttime=timer();
 
 # reading commands for per host execution
 open(FD,$cmdsfile) or die "$!\n";
@@ -45,18 +50,29 @@ while (<FD>){
 
   if (fork == 0){
     chomp;
+
     my $host=$_;
- 
+
+    if (/^(.+?)@(.?):(.+?)$/) {
+      $username=$1;
+      $password=$2;
+      $host=$3;
+    }
+
     my $exp = Expect->new();
 
-    $exp = Expect->spawn("ssh -t -l $username $host 2>&1");
+    $exp->raw_pty(1);
+    $exp = Expect->spawn("ssh -t -oControlMaster=auto -oLoglevel=ERROR -oTCPkeepalive=no -l $username $host");
 
     # no output from the expect session
     $exp->log_user(0);
 
+    # logging everything to a file
+    $exp->log_file($starttime."_".$host.".log");
+
     # logging process
     $exp->expect($timeout,
-      ['eof' => sub { print "$host - connection interrupted\n";
+      ['eof' => sub { print "$host - problem with connecting to the device\n";
                       exit;}],
       ['timeout' => sub { print "$host - connection timeout\n";
                           exit;}],
@@ -74,9 +90,8 @@ while (<FD>){
                               $e->send("$password\r");
                               exp_continue;}],
       [qr/.*?-> / => sub {  my $e=shift;
-                          $e->send("\r");}]
+                            $e->send("\r");}]
     );
-
 
     # running commands
     foreach my $cmd (@cmds){
@@ -124,5 +139,22 @@ while (<FD>){
 }
 close(FD);
 
-# waiting before exit, for all kids running around 
+# waiting before exit for all the kids running around 
 while(wait>0){ }
+
+######### end of main
+
+sub timer {
+  my ($sec, $min, $hour, $mday, $mon, $year);
+  ( $sec, $min, $hour, $mday, $mon, $year, undef, undef, undef ) = localtime(time);
+  $year += 1900;
+  $mon  += 1;
+  $hour = "0$hour" if $hour < 10;
+  $mday = "0$mday" if $mday < 10;
+  $mon  = "0$mon"  if $mon < 10;
+  $min  = "0$min"  if $min < 10;
+  $sec  = "0$sec"  if $sec < 10;
+  my $ret = $year . "-" . $mon . "-" . $mday . "_" . $hour . "-" . $min . "-" . $sec;
+  return $ret;
+}
+

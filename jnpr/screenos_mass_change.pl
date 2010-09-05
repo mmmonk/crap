78ec58b72351cd3554bf9ed2b54360bec6009b65
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2010-09-05 06:50:49 +0000

    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@159 aaeee498-0047-0410-bd38-07bdd6096719

--- jnpr/screenos_mass_change.pl
+++ jnpr/screenos_mass_change.pl
@@ -4,12 +4,18 @@
 
 use strict;
 use warnings;
-#use integer;
+use integer;
 
 use Expect;
 use POSIX qw(:termios_h);
 
-my $maxkids=5;
+# max children running
+my $maxkids=64;
+# timeout for the spawn in expect process
+my $timeout=30;
+
+my $username="netscreen";
+my $password="netscreen";
 
 my $hostsfile=shift;
 my $cmdsfile=shift;
@@ -39,21 +45,84 @@ while (<FD>){
 
   if (fork == 0){
     chomp;
-
+    my $host=$_;
+ 
     my $exp = Expect->new();
 
+    $exp = Expect->spawn("ssh -t -l $username $host 2>&1");
+
+    # no output from the expect session
+    $exp->log_user(0);
+
+    # logging process
+    $exp->expect($timeout,
+      ['eof' => sub { print "$host - connection interrupted\n";
+                      exit;}],
+      ['timeout' => sub { print "$host - connection timeout\n";
+                          exit;}],
+      [qr/you sure you want to continue connecting/ => sub {  my $e=shift;
+                                                              $e->send("yes\n");
+                                                              exp_continue;}],
+      [qr/RSA modulus too small: \d+ < minimum 768 bits/ => sub {print "$host - too small RSA key.\n";
+                                                                exit;}],
+      [qr/Permission denied, please try again/ => sub { print "$host - wrong credentials\n";
+                                                        exit;}],
+      [qr/login:/ => sub {  my $e=shift;
+                            $e->send("$username\r");
+                            exp_continue;}],
+      [qr/assword:/ => sub {  my $e=shift;
+                              $e->send("$password\r");
+                              exp_continue;}],
+      [qr/.*?-> / => sub {  my $e=shift;
+                          $e->send("\r");}]
+    );
+
+
+    # running commands
     foreach my $cmd (@cmds){
-      print "$_ - $cmd\n";
-      sleep 3;
+      $exp->expect($timeout,
+        ['eof' => sub { print "$host - connection interrupted\n";
+                        exit;}],
+        ['timeout' => sub { print "$host - connection timeout\n";
+                            exit;}],
+        [qr/--- more ---/ => sub {  my $e=shift;
+                                    $e->send(" ");
+                                    exp_continue;}],
+        [qr/.*?-> / => sub {  my $e=shift;
+                              $e->send("$cmd\r");}]
+      ); 
     }
+    
+    # exiting
+    $exp->expect($timeout,
+      ['eof' => sub { print "$host - done\n";
+                      exit;}],
+      ['timeout' => sub { print "$host - connection timeout\n";
+                          exit;}],
+      [qr/--- more ---/ => sub {  my $e=shift;
+                                  $e->send(" ");
+                                  exp_continue;}],
+      [qr/.*?-> / => sub {  my $e=shift;
+                            $e->send("exit\r");
+                            exp_continue;}],
+      [qr/Configuration modified, save\?/ => sub {  my $e=shift;
+                                                    $e->send("n");
+                                                    exp_continue;}]
+    ); 
+    
     exit;
 
   }else{
+
+    # birth control ;) 
+    # making sure that we don't have too many kids
     if ($kids>=$maxkids){
       my $pid=wait();
       $kids--;
     }
   }
 }
-while(wait>0){
-}
+close(FD);
+
+# waiting before exit, for all kids running around 
+while(wait>0){ }

921c18c7081471490964355b58ba857276342237
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2010-10-02 05:46:25 +0000

    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@167 aaeee498-0047-0410-bd38-07bdd6096719

--- crap/snmptest.rb
+++ crap/snmptest.rb
@@ -10,7 +10,6 @@ unless ARGV[0]
   exit
 end
 
-query = Array.new 
 snmpcomm = ""
 snmpcomma = ["public","private"]
 
@@ -26,57 +25,80 @@ hosts.push(ARGV[0])
 
 hosts.each do |host|
 
-  query = ["sysContact.0","sysUpTime.0","sysDescr.0","sysName.0","sysLocation.0"]
+  query = ["1.3.6.1.2.1.1.1","1.3.6.1.2.1.1.2","1.3.6.1.2.1.1.3","1.3.6.1.2.1.1.4","1.3.6.1.2.1.1.5","1.3.6.1.2.1.1.6","1.3.6.1.2.1.1.7",]
  
   gotanswer = 0
+
   snmpcommh.sort{|a,b| b[1]<=>a[1]}.each do |comm,value|
     puts "[+] host #{host} community #{comm}\n"
     
-    begin
-      SNMP::Manager.open(:Host => host, :Version => :SNMPv2c, :Community => comm, :Timeout => 5, :Retries => 3) do |manager| 
-        response = manager.get_bulk(0,1,query)
-        list = response.varbind_list
+    manager = SNMP::Manager.new(:Host => host, :Version => :SNMPv2c, :Community => comm, :Timeout => 5, :Retries => 3)
 
-        if not list.empty? 
-          list.each do |ans| 
-            data.push(ans)
-          end
-          gotanswer = 1 
-          snmpcommh[comm] += 1
-          snmpcomm = comm
-        end
-      end
+    begin
+      response = manager.get_bulk(0,1,query)
     rescue
       puts "[-] problem while connecting to host #{host}: #{$!}\n"
     end
-    break if gotanswer == 1
-  end
 
-  if gotanswer == 1 
-    data.each do |item|
-      puts "#{item.oid} - #{item.value}\n"
+    list = response.varbind_list
+
+    if not list.empty? 
+      list.each do |ans| 
+        data.push(ans)
+      end
+      gotanswer = 1 
+      snmpcommh[comm] += 1
+      snmpcomm = comm
     end
-  end
 
-  puts "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n"
+    if gotanswer == 1 
 
-  # next-hops from the routing table
-  query = ["1.3.6.1.2.1.4.24.4.1.4","1.3.6.1.2.1.4.24.4.1.7"]
+      data.each do |item|
+        puts "#{item.oid} - #{item.value}\n"
+      end
 
-  begin
-    SNMP::Manager.open(:Host => host, :Version => :SNMPv2c, :Community => snmpcomm, :Timeout => 5, :Retries => 3) do |manager|
-      manager.walk(query) do |row|
-        puts "#{row[0].value}" if row[1].value >= 3 
+      puts "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n"
+
+      # next-hops and type of the route from the routing table
+      query = ["1.3.6.1.2.1.4.24.4.1.4","1.3.6.1.2.1.4.24.4.1.7"]
+   
+      begin
+          manager.walk(query) do |row| 
+            if row[1].value >= 3
+              puts "#{row[0].value}"
+            else
+              #puts "other #{row[0].value}"
+            end
+          end
+      rescue
+          puts "[-] problem while querying the host #{host}: #{$!}\n"
       end
 
       # IP addresses from the ARP table
       query = ["1.3.6.1.2.1.4.22.1.3"]
-      
-      manager.walk(query) do |row|
-        row.each { |vb| puts "#{vb.value}\n" }
+
+      rows = []      
+      begin 
+        manager.walk(query) do |row|
+          row.each { |vb| puts "#{vb.value}\n" }
+        end
+      rescue
+        puts "[-] problem while querying the host #{host}: #{$!}\n"
       end
+
+      # my ips
+      query = ["1.3.6.1.2.1.3.1.1.3"]
+    
+      rows = []
+      begin
+        manager.walk(query) do |row|
+          row.each { |vb| puts "#{vb.oid} #{vb.value}\n" }
+        end
+      rescue
+        puts "[-] problem while querying the host #{host}: #{$!}\n"
+      end
+ 
+      break if gotanswer == 1
     end
-  rescue
-    puts "[-] problem while querying the host #{host}: #{$!}\n"
   end
 end

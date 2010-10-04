ebdb066eaafaeb852e938ab5d0222fe20dd06a5b
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2010-10-04 14:20:29 +0000

    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@169 aaeee498-0047-0410-bd38-07bdd6096719

--- snmp/snmptest.rb
+++ snmp/snmptest.rb
@@ -10,18 +10,32 @@ unless ARGV[0]
   exit
 end
 
-snmpcomm = ""
 snmpcomma = ["public","private"]
+
+
+# SNMP walking routine, with some added features
+# delay after higher number of queries
 
 def dowalk(mngr,query)
 
   rows = Array.new
-  begin
-    mngr.walk(query) do |row|
-      row.each { |item| rows.push(item.value)}
+
+  count = 0
+
+  query.each do |oid|
+    start_oid = SNMP::ObjectId.new(oid)
+    next_oid = start_oid
+    while next_oid.subtree_of?(start_oid)
+      response = mngr.get_next(next_oid)
+      varbind = response.varbind_list.first
+      break if not varbind.oid.subtree_of?(start_oid)
+      next_oid = varbind.oid
+      count += 1
+      # need to make this as a variable
+      rows.push(varbind.value)
+      sleep 0.1 if count >= 100
+      break if count >= 1000
     end
-  rescue
-    puts "[-] error while querying for #{query.to_s}: #{$!}"
   end
 
   rows
@@ -91,7 +105,11 @@ hosts.each do |host|
         # next-hops and type of the route from the routing table
         query = ["1.3.6.1.2.1.4.24.4.1.4","1.3.6.1.2.1.4.24.4.1.7"]
         data = dowalk(manager,query)
-        (data.length/2).times { |i| j=(i-1)*2;puts data[j] if data[j+1] >=3 }
+
+        # we have two collums here, we are checking if any value 
+        # in the second collumn is bigger or equal 3
+        j=(data.length/2).to_i 
+        (data.length/2).to_i.times { |i| puts data[i] if data[i+j] >=3 }
   
         # IP addresses from the ARP table
         query = ["1.3.6.1.2.1.4.22.1.3"]

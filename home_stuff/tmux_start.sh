e512a2f9c5d419a1f5439f7a2db3a28582ad59d0
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2010-11-16 18:20:50 +0000

    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@183 aaeee498-0047-0410-bd38-07bdd6096719

--- home_stuff/tmux_start.sh
+++ home_stuff/tmux_start.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
-tmux has-session -t $1
+if [ -n $1 ]; then
+  tmux start
 
-if [ $? = 0 ]; then
-  tmux attach -t $1 
-else
-  if [ $1 = $0 ]; then
-    tmux new-session -n 0 \; \
-      rename-window -t 0 ekg \; \
-      new-window -d -n tao -t 1 \; \
-      split-window -d -h -t 1 'mutt' \; \
-      new-window -d -n rudy -t 2 'ssh_keep.sh r' \; \
-      new-window -d -n xls -t 3 'ssh_keep.sh x' \; \
-      new-window -d -n logs -t 4 'sudo tail --follow=name /var/log/messages' \; \
-      split-window -d -h -t 4 'sudo tail --follow=name /var/log/auth.log | grep -v CRON' \; \
-      new-window -d -t 5 \; \
-      setw -t 4 monitor-activity on
+  tmux has-session -t $1
+
+  if [ $? = 0 ]; then
+    tmux attach -t $1 
   else
-    tmux new-session -n $1
+    if [ $1 = 0 ]; then
+      tmux new-session -n 0 \; \
+        rename-window -t 0 ekg \; \
+        new-window -d -n tao -t 1 \; \
+        split-window -d -h -t 1 'mutt' \; \
+        new-window -d -n rudy -t 2 'ssh_keep.sh r' \; \
+        new-window -d -n xls -t 3 'ssh_keep.sh x' \; \
+        new-window -d -n logs -t 4 'sudo tail --follow=name /var/log/messages' \; \
+        split-window -d -h -t 4 'sudo tail --follow=name /var/log/auth.log | grep -v CRON' \; \
+        new-window -d -t 5
+      tmux setw -t 4 'monitor-activity on'
+    else
+      tmux new-session -n $1
+    fi
   fi
 fi

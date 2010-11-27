06945e7bfdf0f04a2b3dfba3f7425c1c4b565aa8
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2010-11-27 12:36:34 +0000

    made I/O operations non-blocking, with predefined timeout
    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@212 aaeee498-0047-0410-bd38-07bdd6096719

--- tcp_and_proxy/socket_test.tcl
+++ tcp_and_proxy/socket_test.tcl
@@ -4,74 +4,91 @@
 # $Id$
 #
 
-set socketimeout 500 ;# in ms
-
 if { $argc != 2 } {
   puts "usage: $argv0 host port"
   exit
 }
+
 set host [lindex $argv 0]
 set port [lindex $argv 1]
 
-# procedure to be used in fileevent
+set sockettimeout 500 ;# in ms
+# to get value bellow do: openssl s_client -debug -msg -ssl3 -connect google.com:443 | head -n 20
+set sslclient_hello "16030000540100005003004cf0a8b1885a4d8dd81347e1a3dfc98a7e4bdb126a44b5b969f10c219627a4e800002800390038003500160013000a00330032002f000500040015001200090014001100080006000300ff02010"
+set sslhello_length  [ string length $sslclient_hello ]
+
+########################
+# procedure to be used in fileevent to test TCP
+#
 proc stok {sock} {
-  global state
   
   # we could get here because of the error
   set resp [fconfigure $sock -error]
 
   if {$resp == "" } { 
-    set state ok
-  } else {
-    set state closed 
-  }
+    return "tcp:ok" 
+  } 
+  return "tcp:closed" 
 }
 
-# set the timeout value
-after $socketimeout set state timeout 
+########################
+# procedure to be used in fileevent to test SSL
+#
+proc sslok {sock} {
 
-# do non-blocking connect
-set sock [socket -async $host $port ] 
-
-# wait for the socket to be writetable
-fileevent $sock writable { stok $sock }
-
-# otherwise timeout here
-vwait state
-
-after cancel set state timeout 
-
-puts $state
-
-if { $state == "ok" } {
-
-  fconfigure $sock -blocking on
-
-  # to get value bellow do: openssl s_client -debug -msg -ssl3 -connect google.com:443 | head -n 20
-  
-  set client_hello "16030000540100005003004cf033aeb88cf0efec4a0b0f68eaaa666f2b65d06654dfdcd1ad9bd3bd1b507f00002800390038003500160013000a00330032002f000500040015001200090014001100080006000300ff0201"
-
-  set hello_length  [ string length $client_hello ]
-  puts -nonewline $sock [ binary format "H${hello_length}" $client_hello ]
-  flush $sock
-  
   set lines [ read $sock 100 ]
 
-  if { ![ binary scan $lines "@0H6" res ] } {
-
-    puts "ssl failed"
-
-  } else { 
-    if { $res == "150300" } {
-    
-      puts "got correct ssl server handshake"
-
-    } else {
-
-      puts "I don't understand the answer from the server"
-
-    }
+  if { ![ binary scan $lines "@0H6" resp ] } {
+    return "tcp:ok, ssl:binary scan error"
   }
+  
+  if { $resp == "150300" } {
+    return "ok"
+  } 
+
+  return "tcp:ok, ssl:bad response"
 }
+
+########################
+### main
+########################
+
+# set the timeout value
+after $sockettimeout set tcpstate "tcp:timeout" 
+
+# create a non-blocking socket 
+set sock [socket -async $host $port ] 
+
+# read needs this explicitly defined, strange
+fconfigure $sock -blocking no
+
+# wait for the socket to be writable
+fileevent $sock writable { set tcpstate [stok $sock ] } 
+
+# otherwise timeout here
+vwait tcpstate
+
+after cancel set tcpstate "tcp:timeout" 
+
+set state $tcpstate
+
+if { $tcpstate == "tcp:ok" } {
+
+  # we send predefine ssl client hello here - simple, but it works
+  puts -nonewline $sock [ binary format "H${sslhello_length}" $sslclient_hello ]
+  flush $sock
+
+  after $sockettimeout set sslstate "ssl:timeout" 
+  
+  fileevent $sock readable { set sslstate [sslok $sock ] }
+
+  vwait sslstate
+
+  after cancel set sslstate "ssl:timeout" 
+
+  set state $sslstate
+}
+
+puts $state
 
 close $sock

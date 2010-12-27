5e97a4a2da3f74c76991806b6441b4b36acefb2a
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2010-12-27 11:19:03 +0000

    nsm schema check and download - filling out the form on the website
    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@216 aaeee498-0047-0410-bd38-07bdd6096719

--- jnpr/nsm/nsmschemadownload.py
+++ jnpr/nsm/nsmschemadownload.py
@@ -2,8 +2,79 @@
 
 # $Id$
 
-import httplib
-import os
-import time
+import cookielib
+import urllib
+import urllib2 
+import sgmllib 
+import sys
 
-# https://download.juniper.net/software/nsm/JuniperUpdate/NSM-schema-latest.tar.gz
+conffile='/home/case/.nsmauth.conf'
+
+def LoadConf(filename):
+
+  global confvar
+
+  confvar = {} 
+  try:
+    conf=open(filename,'r')
+  except:
+    print "error during conf read"
+    sys.exit(1)
+
+  line = conf.readline()
+
+  while line:
+    conft = line.replace('\n','').split("=")
+    confvar[conft[0]]=conft[1]
+    line = conf.readline() 
+
+
+class FormParser(sgmllib.SGMLParser):
+  "A simple parser class."
+
+  def parse(self, s):
+    "Parse the given string 's'."
+    self.feed(s)
+    self.close()
+
+  def __init__(self, verbose=0):
+    sgmllib.SGMLParser.__init__(self, verbose)
+    self.form = {} 
+    self.inside_auth_form = 0
+
+  def do_input(self, attributes):
+    if self.inside_auth_form == 1:
+      if 'hidden' in attributes[0]:
+        self.form[attributes[1][1]]=attributes[2][1] 
+
+  def start_form(self, attributes):
+    for name, value in attributes:
+      if name == "name" and value == "Login":
+        self.inside_auth_form=1
+        break
+
+  def end_form(self):
+    self.inside_auth_form=0    
+
+  def get_form(self):
+    return self.form
+
+LoadConf(conffile)
+
+url=confvar['schemalink']
+
+cj = cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+urllib2.install_opener(opener)
+
+dat = urllib2.urlopen(url)
+parser = FormParser()
+parser.parse(dat.read())
+
+form = parser.get_form()
+form['USER']=confvar['schemauser']
+form['PASSWORD']=confvar['schemapass']
+params = urllib.urlencode(form)
+dat2 = urllib2.urlopen(dat.geturl(),params)
+dat3 = urllib2.urlopen(dat2.geturl()) 
+print dat3.info()['Last-Modified']+"\n"+dat3.info()['Content-Length']

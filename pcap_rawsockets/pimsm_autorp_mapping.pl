1e2b900ec945328768bb12d41736306f7602829f
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2011-04-16 15:37:30 +0000

    pim-sm auto-rp packet generator
    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@240 aaeee498-0047-0410-bd38-07bdd6096719

---
+++ pcap_rawsockets/pimsm_autorp_mapping.pl
@@ -0,0 +1,139 @@
+#!/usr/bin/perl
+
+# $Id$
+
+use strict;
+use warnings;
+use integer;
+
+use Net::RawIP;
+
+
+my ($src,$dst,$rp);
+
+# some defaults values
+#
+my $sleep=1;
+my $count=1;
+my $htime=181;
+my $ttl=5;
+my @gaddr=();
+
+sub usage{
+  print "
+usage: $0 -d <destination IP> -rp <rendezvous point IP> <more options>
+
+  packet is being send according to:
+  ftp://ftp.icm.edu.pl/packages/cisco-ipmulticast/pim-autorp-spec01.txt
+
+  options:
+  -d <IP>
+    destination IP for the packet - required,
+
+  -rp <IP>
+    rendezvous point IP - required,
+
+  -g <groupaddress/bitmask>
+    group address, can be specified more then once, if prefixed 
+    with '-' (minus) this will be a negative group address, 
+    if not set the default value is: -224.0.0.0/4 
+
+  -s <IP>
+    source IP, by default handled by the OS,
+
+  -ht <seconds>
+    holddown timer, default value is ".$htime." seconds,
+
+  -t <ttl>
+    TTL value of the IP packet, default value is ".$ttl.",
+
+  -c <integer>
+   number of packets to send, default value is ".$count.",
+
+  -i <seconds>
+   how long to wait in seconds between sending each packet,
+   default value is ".$sleep.",
+
+\n";
+  exit;
+}
+
+usage if ($#ARGV==0);
+
+for(my $i=0;$i<$#ARGV;$i+=2){
+  if($ARGV[$i] eq "-s" and exists($ARGV[$i+1])){ $src=$ARGV[$i+1]; next;}
+  if($ARGV[$i] eq "-d" and exists($ARGV[$i+1])){ $dst=$ARGV[$i+1]; next;}
+  if($ARGV[$i] eq "-rp" and exists($ARGV[$i+1])){ $rp=$ARGV[$i+1]; next;}
+  if($ARGV[$i] eq "-ht" and exists($ARGV[$i+1])){ $htime=$ARGV[$i+1]; next;}
+  if($ARGV[$i] eq "-t" and exists($ARGV[$i+1])){ $ttl=$ARGV[$i+1]; next;}
+  if($ARGV[$i] eq "-c" and exists($ARGV[$i+1])){ $count=$ARGV[$i+1]; next;}
+  if($ARGV[$i] eq "-i" and exists($ARGV[$i+1])){ $sleep=$ARGV[$i+1]; next;}
+  if($ARGV[$i] eq "-g" and exists($ARGV[$i+1])){ push(@gaddr,$ARGV[$i+1]); next;}
+  usage; 
+}
+
+usage if (!defined($dst) or !defined($rp));
+
+if($#gaddr==-1){
+  push(@gaddr,"-224.0.0.0/4");
+}
+
+# payload definition 
+# ftp://ftp.icm.edu.pl/packages/cisco-ipmulticast/pim-autorp-spec01.txt
+
+# version 1 type 2 | rp count 1
+my @data = (     
+  0x12,0x01
+);
+
+if ($htime>255){
+  push(@data,($htime/256));
+}else{
+  push(@data,0);
+}
+push(@data,($htime%256));
+
+# reservered bits
+push(@data,(
+  0x00,0x00,0x00,0x00  
+));                     
+
+push(@data,split('\.',$rp)); 
+
+# RP highest PIM version - 11 : Dual version 1 and 2 
+push(@data,0x03);
+
+push(@data,$#gaddr+1);
+foreach (@gaddr){
+  if (/^-/){
+    push(@data,0x01);
+    s/^-//;
+  }else{
+    push(@data,0x00);
+  }
+  my @g=split('/');
+  push(@data,$g[1]);
+  push(@data,split('\.',$g[0]));
+}
+
+#my $payload = pack "C*",@hexpayload;
+
+my $a= Net::RawIP->new(
+  {ip => {daddr => $dst, ttl=>$ttl},
+  udp => {source => 496,dest => 496, data => pack "C*",@data}}
+);
+
+if (defined($src)){
+  $a->set({ip=>{saddr=>$src}});
+}
+
+$count--;
+if ($count>0){
+  for (1..$count){
+    print localtime(time())." sending packet dst:$dst rp:$rp\n";
+    $a->send();
+    sleep $sleep;
+  }
+}
+print localtime(time())." sending packet dst:$dst rp:$rp\n";
+$a->send();

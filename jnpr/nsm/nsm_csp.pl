b6dbdff68b9830e1a4f54aff5f469672bf2f1e9f
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2011-06-03 12:21:42 +0000

    more features, less bugs - nsm_csp.pl
    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@291 aaeee498-0047-0410-bd38-07bdd6096719

--- jnpr/nsm/nsm_csp.pl
+++ jnpr/nsm/nsm_csp.pl
@@ -8,6 +8,7 @@ use HTML::Entities;
 
 my $homedir="/home/case/store/jj/nsmdiff_and_stuff";
 my $dbfile="/home/www/perl/nsm_csp.tsv";
+my $latest="/home/www/perl/nsm_csp_latest.tsv";
 
 # NSM version sorting
 sub nsm_sorting {
@@ -21,9 +22,29 @@ sub nsm_sorting {
   ($bb[2] = $b ) =~ s/LGB\d+\D+\d+(\D+).*/$1/;
   ($bb[3] = $b ) =~ s/LGB\d+\D+\d+\D+(\d+).*/$1/;
    
-  $bb[0] <=> $aa[0] || $bb[1] <=> $aa[1] || $bb[2] cmp $aa[2] || $bb[3] <=> $aa[3];
+  return $bb[0] <=> $aa[0] || $bb[1] <=> $aa[1] || $bb[2] cmp $aa[2] || $bb[3] <=> $aa[3];
 }
 
+sub columns {
+  my $a=shift;
+  my $b=shift;
+  my $col=shift;
+  my $ord=shift;
+
+  if ($col<=1){
+    if ($ord==0){
+      return 0;
+    }else{
+      return 1;
+    }
+  }
+  
+  if ($ord==0){
+    return (split("\t",$a))[$col] cmp (split("\t",$b))[$col];
+  } else {
+    return (split("\t",$b))[$col] cmp (split("\t",$a))[$col];
+  }
+}
 
 # getting GET values
 my %params=map{my($name,$value)=split/\=/;$name => $value} map{split /\&/} $ENV{"QUERY_STRING"};
@@ -33,6 +54,7 @@ if ( ! -e $dbfile or ! -e $homedir."/latest_update.txt" or (stat($dbfile))[9] < 
   my %nsm;
   my %all;
   my %all_descr;
+  my %tsnsmpatch;
 
   #loading NSM naming file
   open(FD,"/home/www/perl/nsm_naming.txt") or die "can't load file\n";
@@ -61,8 +83,10 @@ if ( ! -e $dbfile or ! -e $homedir."/latest_update.txt" or (stat($dbfile))[9] < 
       my $prntdsc=0;
       my $prdesc=" ";
       open(FD,$homedir."/".$nsmver."/".$diff);
+      $tsnsmpatch{$nsmver}=(stat(FD))[9];
       while(<FD>){
         chomp;
+        s/\t//g;
         next if (/^<\s+$/);
         if (/^<\s+(Bug:|\/B:|Title:)\s*(.+?)$/){
           my $f=$2;
@@ -109,7 +133,7 @@ if ( ! -e $dbfile or ! -e $homedir."/latest_update.txt" or (stat($dbfile))[9] < 
     }
     closedir(DIFF);
   }
-  open(DB,"> /home/www/perl/nsm_csp.tsv") or die "Can't open file: $!\n";
+  open(DB,"> $dbfile") or die "Can't open file: $!\n";
   foreach my $nsmpr (sort nsm_sorting keys %all){
     my ($nsmver,$pr) = split/#/,$nsmpr;
     my $mainver="";
@@ -118,11 +142,17 @@ if ( ! -e $dbfile or ! -e $homedir."/latest_update.txt" or (stat($dbfile))[9] < 
     }
     $all{$nsmpr}=HTML::Entities::encode($all{$nsmpr});
     $all_descr{$nsmpr}=HTML::Entities::encode($all_descr{$nsmpr});
-    $all{$nsmpr}=~s/\n/<br\/>/g;
-    $all_descr{$nsmpr}=~s/\n/<br\/>/g;
-    print DB "$nsmver\t$mainver\t$pr\t$all{$nsmpr}\t$all_descr{$nsmpr}\n";
+    $all{$nsmpr}=~s/\n/<br\/>/sg;
+    $all_descr{$nsmpr}=~s/\n/<br\/>/sg;
+    print DB "$nsmver\t$mainver\t$pr\t$all{$nsmpr}\t$all_descr{$nsmpr}\t$tsnsmpatch{$nsmver}\n";
   }
   close(DB);
+  my @sorted=reverse sort {$tsnsmpatch{$a} <=> $tsnsmpatch{$b} } keys %tsnsmpatch;
+  open(TOP,"> $latest") or die "Can't open file: $!\n";
+  for (my $i=0;$i<=9;$i++){
+    print TOP $sorted[$i]."\n";
+  }
+  close(TOP);
 }
 
 my $pagetitle="patch list with fixes for NSM";
@@ -132,14 +162,14 @@ print "Content-type: text/html\n\n\
 <html>\
 <head>\
 <title>$pagetitle</title>\
-<STYLE TYPE=\"text/css\">
+<style type=\"text/css\">
 <!--
 body {background-color:#CCCCCC;}
-.head1, .head1 TD, .head1 TH { background-color:black; color:#CCCCCC;}
+.head1, .head1 TD, .head1 TH A { background-color:black; color:#CCCCCC;}
 .row1, .row1 TD, .row1 TH { background-color:#99CCFF; color:#000000;}
 .row2, .row2 TD, .row2 TH { background-color:#99FFFF; color:#000000;}
 -->
-</STYLE>
+</style>
 </head>\
 <body> 
 <h1 align=\"center\"><a href=\"?\">$pagetitle</a></h1>\n";
@@ -167,19 +197,50 @@ if (exists($params{'q'}) and $params{'q'} ne ""){
 print "</form>\
 </table><br/>";
 
-print "<br/><table CLASS=\"head1\" width=\"90%\" border=\"1\" frame=\"border\" align=\"center\">\
-<tr><th>patch</th>\
-<th>PR#</th>\
-<th>PR title</th>\
-<th>Change description</th></tr>\n";
+print "<p align=\"center\"><b> Ten newest releases: ";
+open(TOP,$latest) or die "$!";
+while(<TOP>){
+  chomp;
+  print "<a href=\"?q=$_&m=1\">$_</a> ";
+}
+close(TOP);
+print "</b></p>\n";
 
-my $color=1;
+if (exists($params{'o'}) and $params{'o'} == 0) {
+  $params{'o'}=1;
+}else{
+  $params{'o'}=0;
+}
+
+$ENV{"QUERY_STRING"}=~s/(&c=\d+|&o=\d+)//g;
+
+
+print "<br/><table CLASS=\"head1\" width=\"90%\" border=\"1\" frame=\"border\" align=\"center\">\
+<tr><th><a href=\"?".$ENV{"QUERY_STRING"}."&c=1&o=".$params{'o'}."\">patch</a></th>\
+<th><a href=\"?".$ENV{"QUERY_STRING"}."&c=2&o=".$params{'o'}."\">PR#</a></th>\
+<th><a href=\"?".$ENV{"QUERY_STRING"}."&c=3&o=".$params{'o'}."\">PR title</a></th>\
+<th><a href=\"?".$ENV{"QUERY_STRING"}."&c=4&o=".$params{'o'}."\">Change description</a></th>
+<th><a href=\"?".$ENV{"QUERY_STRING"}."&c=5&o=".$params{'o'}."\">Download date</a></th></tr>\n";
+
+my @db;
 open(DB,$dbfile) or die "$!";
 while(<DB>){
-  my ($nsmver,$mainver,$pr,$prtitle,$prdesc) = split("\t");
+  my ($nsmver,undef,$pr,$prtitle,$prdesc,undef) = split("\t");
   next if (exists($params{'q'}) and $params{'q'} ne "" and $nsmver!~/$params{'q'}/i);
   next if (exists($params{'prn'}) and $params{'prn'} ne "" and $pr!~/$params{'prn'}/);
   next if (exists($params{'prt'}) and $params{'prt'} ne "" and $prtitle!~/$params{'prt'}/i and $prdesc!~/$params{'prt'}/i);
+  push(@db,$_);
+}
+close(DB);
+
+if (!exists($params{'c'})){
+  $params{'c'}=1;
+}
+
+my $color=1;
+foreach my $line (sort {columns($a,$b,$params{'c'},$params{'o'})} @db){
+  chomp $line;
+  my ($nsmver,$mainver,$pr,$prtitle,$prdesc,$ts) = split("\t",$line);
   if ($color==1){
     print "<tr CLASS=\"row1\">";
     $color=0;
@@ -191,9 +252,9 @@ while(<DB>){
   if ($pr=~/^\d+\s*$/ and $pr > 0) { 
     print "<a href=\"https://gnats.juniper.net/web/default/$pr\">";
   }
-  print "$pr</a></td><td>".$prtitle."</td><td>".$prdesc."</td></tr>\n";
+  my @date=localtime($ts);
+  print "$pr</a></td><td>".$prtitle."</td><td>".$prdesc."</td><td>".(1900+$date[5])."-".(1+$date[4])."-".$date[3]."</td></tr>\n";
 }
-close(DB);
 
 print "</table>\n";
 if (exists($params{'m'}) and $params{'m'}==1 and exists($params{'q'}) and $params{'q'} ne ""){
@@ -204,6 +265,6 @@ if (exists($params{'m'}) and $params{'m'}==1 and exists($params{'q'}) and $param
     close(DIFF);  
   } 
 }
-print "<p align=\"center\"><a href=\"mailto:mlukaszuk\@juniper.net\">complains/ideas</a></p></body></html>\n";
+print "<p align=\"center\"><a href=\"mailto:mlukaszuk\@juniper.net\">complains/ideas</a><br/>This file was last modifed at: ".localtime((stat($0))[9])."</p></body></html>\n";
 
 

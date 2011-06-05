ae15d586c9b76814eac44d115b6bf52cb8dcba9f
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2011-06-05 15:48:00 +0000

    gauth.py - google totp authentication token generator
    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@292 aaeee498-0047-0410-bd38-07bdd6096719

---
+++ home_stuff/gauth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+#
+# $Id$
+#
+
+from time import time
+from struct import pack,unpack
+from hmac import HMAC 
+from hashlib import sha1
+from base64 import b32decode 
+from os import getenv
+
+s = open(getenv("HOME")+"/.gauth.conf").read()
+
+tm = int(time() / 30)
+
+secretkey = b32decode(s.replace(" ","").rstrip().upper())
+
+# convert timestamp to raw bytes
+b = pack(">q", tm)
+
+# generate HMAC-SHA1 from timestamp based on secret key
+hm = HMAC(secretkey, b, sha1).digest()
+
+# extract 4 bytes from digest based on LSB
+offset = ord(hm[-1]) & 0x0F
+truncatedHash = hm[offset:offset+4]
+
+# get the code from it
+code = unpack(">L", truncatedHash)[0]
+code &= 0x7FFFFFFF;
+code %= 1000000;
+
+print "0"*(6-len(str(code)))+str(code)

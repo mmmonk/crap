8dc92de67221bdaccd8f13eaccc065caa7a15b78
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2011-06-06 08:20:47 +0000

    nsmlogreader.pl - a script to correct timestamp in NSM logs
    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@294 aaeee498-0047-0410-bd38-07bdd6096719

---
+++ jnpr/nsm/nsmlogreader.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+
+# $Id$
+
+use strict;
+use warnings;
+use integer;
+
+my $c=0;
+my $ts="";
+my @data;
+
+my ($date,$time,$d,$m,$Y);
+
+while(<>){
+  if (/^(\d{4}\/\d{2}\/\d{2}-\d{2}:\d{2}:\d{2}\.\d{3}) /){
+    $ts=$1;
+    $c=0;
+    push(@data,"$ts 000 $_");
+    next;
+  }
+  
+  if (/^\[(\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2}\.\d{3})\] /){
+    ($date,$time)=split(" ",$1);
+    ($d,$m,$Y)=split('/',$date);
+    $ts="$Y/$m/$d $time";
+    $c=0;
+    push(@data,"$ts 000 $_");
+    next;
+  }
+  
+  next if ($ts eq "");
+
+  $c++;
+  if ($c<10) {
+    push(@data,"$ts 00$c $_");
+    next;
+  }
+  if ($c<100) {
+    push(@data,"$ts 00$c $_");
+    next;
+  }
+
+  push(@data,"$ts $c $_");
+}
+
+foreach my $line (sort @data){
+  print $line;
+}

c10e828d9ffea7e32403aa0ff1467d3210322ad5
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2011-07-04 10:14:11 +0000

    more then one key is possible
    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@296 aaeee498-0047-0410-bd38-07bdd6096719

--- home_stuff/gauth.py
+++ home_stuff/gauth.py
@@ -13,28 +13,32 @@ from os import getenv
 from sys import exit
 
 try:
-  s = open(getenv("HOME")+"/.gauth.conf").read()
+  fd = open(getenv("HOME")+"/.gauth.conf")
 except:
   print "Can't read ~/.gauth.conf file"
   exit(1)
 
 tm = int(time() / 30)
 
-secretkey = b32decode(s.replace(" ","").rstrip().upper())
+s = fd.read()
+while (s != ""):
+  s = s.split("|")
+  secretkey = b32decode(s[1].replace(" ","").rstrip().upper())
 
-# convert timestamp to raw bytes
-b = pack(">q", tm)
+  # convert timestamp to raw bytes
+  b = pack(">q", tm)
 
-# generate HMAC-SHA1 from timestamp based on secret key
-hm = HMAC(secretkey, b, sha1).digest()
+  # generate HMAC-SHA1 from timestamp based on secret key
+  hm = HMAC(secretkey, b, sha1).digest()
 
-# extract 4 bytes from digest based on LSB
-offset = ord(hm[-1]) & 0x0F
-truncatedHash = hm[offset:offset+4]
+  # extract 4 bytes from digest based on LSB
+  offset = ord(hm[-1]) & 0x0F
+  truncatedHash = hm[offset:offset+4]
 
-# get the code from it
-code = unpack(">L", truncatedHash)[0]
-code &= 0x7FFFFFFF;
-code %= 1000000;
+  # get the code from it
+  code = unpack(">L", truncatedHash)[0]
+  code &= 0x7FFFFFFF;
+  code %= 1000000;
 
-print "0"*(6-len(str(code)))+str(code)
+  print str(s[0])+": "+"0"*(6-len(str(code)))+str(code)
+  s = fd.read()

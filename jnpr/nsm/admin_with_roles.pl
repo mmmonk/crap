1a2841e9e195c96359acda2e26bc3d97f358f9b8
m.lukaszuk <m.lukaszuk@aaeee498-0047-0410-bd38-07bdd6096719>
2011-08-08 09:03:22 +0000

    dbxml: more checks added
    git-svn-id: http://monkey-stuff.googlecode.com/svn/trunk@301 aaeee498-0047-0410-bd38-07bdd6096719

--- jnpr/nsm/admin_with_roles.pl
+++ jnpr/nsm/admin_with_roles.pl
@@ -9,92 +9,77 @@ use integer;
 my $guiutils="/usr/netscreen/GuiSvr/utils/";
 my $dbroot="/usr/netscreen/GuiSvr/var/xdb/data/";
 my $queryfile="/tmp/dbxml_adminrole_query.txt";
-
 my $dbver;
 
-opendir(DIR,$guiutils);
+opendir(DIR,$guiutils) or die "Can't read dir $guiutils.\n";
 while(my $node=readdir(DIR)){
   next unless ($node=~/dbxml/);
   $dbver=$node;
 }
 closedir(DIR); 
 
-my $dbxmlpath="$guiutils/$dbver/";
+die "Can't find valid dbxml version, exiting\n" unless ($dbver); 
 
+my $dbxmlpath="$guiutils/$dbver/";
 $ENV{LD_LIBRARY_PATH}="$dbxmlpath/lib:";
 
-if ( -f $queryfile){
-  unlink($queryfile);
-}
-
 umask(0077);
-open(Q1,"> $queryfile") or die "Can't create file: $queryfile\n";
-print Q1 "openContainer \"admin\"
+
+sub dbxmlquery{
+  my $container=shift;
+  my $query=shift;
+
+  open(Q1,"> $queryfile") or die "Can't create file: $queryfile\n";
+  print Q1 "openContainer \"$container\"
 setQueryTimeout 10
-query 'collection()/__[dbxml:metadata(\"HighDbVerID\")=65520]'
+query 'collection()/$query'
 print
 quit";
-close(Q1);
+  close(Q1);
 
-# <__><![CDATA[&0.role.79]]><domains><__><![CDATA[1]]></__></domains></__>
+  open(CMD,"$dbxmlpath/bin/dbxml -h $dbroot -s $queryfile |") or die "Can't run command: $!";
+  my @data=<CMD>;
+  close(CMD);
+  unlink($queryfile);
+  return @data;
+}
+
+unlink($queryfile) if ( -f $queryfile);
+my @res = dbxmlquery("admin","__[dbxml:metadata(\"HighDbVerID\")=65520]");
 
 my %admin;
 my ($name,$perms);
-open(CMD,"$dbxmlpath/bin/dbxml -h $dbroot -s $queryfile |");
-while(<CMD>){
+foreach (@res){
   next if (/^Joined/);
-  $name=$1 if (/^\<__\>\<\!\[CDATA\[(.+?)\]\]\>/);
-  $perms=$1 if (/\<perms\>(.+?)\<\/perms\>/);
+  $name = $1 if (/^\<__\>\<\!\[CDATA\[(.+?)\]\]\>/);
+  $perms = $1 if (/\<perms\>(.+?)\<\/perms\>/);
   my @roles = $perms=~/CDATA\[\&(\d+\.role\.\d+)\].*?\[CDATA\[(\d+)\]\]/g;
-  $admin{$name}=join(':',@roles);
+  $admin{$name} = join(':',@roles);
 }
-close(CMD);
-unlink($queryfile);
 
-my %rolenames;
-my %domains;
+my (%rolenames,%domains,$tname);
 foreach my $name (sort keys %admin){
-  my @roles=split(":",$admin{$name});
-  my @troles=();
+  my @roles = split(":",$admin{$name});
+  my @trols = ();
   for(my $i=0;$i<scalar(@roles)-1;$i+=2) {
     if (!exists($rolenames{$roles[$i]})){
       (my $objid=$roles[$i])=~s/\d+.role.(\d+)/$1/;
-      open(Q1,"> $queryfile") or die "Can't create file: $queryfile\n";
-      print Q1 "openContainer \"role\"
-setQueryTimeout 5 
-query 'collection()/__[dbxml:metadata(\"ObjectID\")=$objid and dbxml:metadata(\"HighDbVerID\")=65520]'
-print
-quit";
-      close(Q1);
-      open(CMD,"$dbxmlpath/bin/dbxml -h $dbroot -s $queryfile |");
-      my $name=$roles[$i];
-      while(<CMD>){
+      @res = dbxmlquery("role","__[dbxml:metadata(\"ObjectID\")=$objid and dbxml:metadata(\"HighDbVerID\")=65520]");
+      foreach (@res){
         next if (/^Joined/);
-        $name=$1 if (/^\<__\>\<\!\[CDATA\[(.+?)\]\]\>/);
+        $tname=$1 if (/^\<__\>\<\!\[CDATA\[(.+?)\]\]\>/);
       }
-      $rolenames{$roles[$i]}=$name;
-      close(CMD);
-      unlink($queryfile);
+      $rolenames{$roles[$i]}=$tname;
     }
     if (!exists($domains{$roles[$i+1]})){
-      open(Q1,"> $queryfile") or die "Can't create file: $queryfile\n";
-      print Q1 "openContainer \"domain\"
-setQueryTimeout 5
-query 'collection()/__[dbxml:metadata(\"ObjectID\")=".$roles[$i+1]." and dbxml:metadata(\"HighDbVerID\")=65520]'
-print
-quit";
-      close(Q1);
-      open(CMD,"$dbxmlpath/bin/dbxml -h $dbroot -s $queryfile |");
-      my $name=$roles[$i+1];
-      while(<CMD>){
+      @res = dbxmlquery("domain","__[dbxml:metadata(\"ObjectID\")=".$roles[$i+1]." and dbxml:metadata(\"HighDbVerID\")=65520]");
+      foreach (@res){
         next if (/^Joined/);
-        $name=$1 if (/^\<__\>\<\!\[CDATA\[(.+?)\]\]\>/);
+        $tname=$1 if (/^\<__\>\<\!\[CDATA\[(.+?)\]\]\>/);
       }
-      $domains{$roles[$i+1]}=$name;
-      close(CMD);
-      unlink($queryfile);
+      $domains{$roles[$i+1]}=$tname;
     }  
-    push(@troles,"\"".$rolenames{$roles[$i]}."\"@\"".$domains{$roles[$i+1]}."\"");
+    push(@trols,"\"".$rolenames{$roles[$i]}."\"@\"".$domains{$roles[$i+1]}."\"");
   }
-  print "$name - ".(join(",",@troles))."\n";
+  print "$name - ".(join(",",@trols))."\n";
 }

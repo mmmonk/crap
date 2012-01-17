2d2b92e291d40abc5e9a3befcd715683e8b12723
Marek Lukaszuk <m.lukaszuk@gmail.com>
2012-01-17 12:11:37 +0100

    other/rfc.py - automatically fetches what is needed and store the files locally

--- other/rfc.py
+++ other/rfc.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+from subprocess import call
 import sys
+import os
+import time
 
-rfcdir = "/home/case/store/docs/ftp.ietf.org/rfc/"
-rfcindex = rfcdir+"/rfc-index"
+rfcdir = "/home/case/store/rfc/"
+
+wget = "/usr/bin/wget"
 
 if len(sys.argv) < 2:
   print "usage: "+sys.argv[0]+" (pattern_to_search_for|rfc_number)"
@@ -11,15 +15,29 @@ if len(sys.argv) < 2:
 
 query = sys.argv[1]
 
+try:
+  os.mkdir(rfcdir,0700)
+except:
+  pass
+
 if query.isdigit():
   try:
-    print open(rfcdir+"/rfc"+query,"r").read()
+    try:
+      print open(rfcdir+"/rfc"+query,"r").read()
+    except IOError:
+      call(wget+" -m -q -t 2 -O "+rfcdir+"/rfc"+query+" https://tools.ietf.org/rfc/rfc"+query+".txt", shell=True)
+      print open(rfcdir+"/rfc"+query,"r").read()
   except:
     print "can't open rfc"+query
 
 else:
   try:
-    rfc = open(rfcindex,"r")
+    try:
+      if (int(time.time())-int(os.stat(rfcdir+"/rfc-index").st_mtime))/3600 > 3600*24*7:
+        call(wget+" -q -t 3 -O "+rfcdir+"/rfc-index ftp://ftp.ietf.org/rfc/rfc-index",shell=True)
+    except OSError, e:
+      call(wget+" -q -t 3 -O "+rfcdir+"/rfc-index ftp://ftp.ietf.org/rfc/rfc-index",shell=True)
+    rfc = open(rfcdir+"/rfc-index","r")
   except:
     print "can't open rfc index"
     sys.exit(1)

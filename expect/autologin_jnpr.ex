28ba250a36aff264174d98b9283d98d98d6500b7
Marek Lukaszuk <m.lukaszuk@gmail.com>
2012-02-11 11:26:46 +0100

    Expect scripts corrections

--- expect/autologin_jnpr.ex
+++ expect/autologin_jnpr.ex
@@ -1,1 +1,216 @@
-./autologin/autologin_jnpr.ex
+#!/usr/bin/expect --
+
+;# $Id$
+
+;# package require base64
+
+;# set send_slow  {2  .001}
+set send_slow {10 .01}
+set timeout 60
+
+if { $argc == 0 } {
+  exit
+} else {
+  set host [lindex $argv 0]
+  set port "23"
+  set name [lindex $argv 0]
+  if { $argc > 1 } {
+    set port [lindex $argv 1]
+  }
+  if { $argc > 2 } {
+    set name [lindex $argv 2]
+  }
+}
+
+set pass "netscreen"
+set enab "netscreen"
+set user "netscreen"
+
+set prompt "*-> "
+
+send_user "CTRL+a
+c - basic config
+i - show ip address
+l - auto login
+d - quit
+"
+
+;# we are connecting to the remote host
+spawn telnet $host $port
+send -s "\r"
+
+expect timeout {
+	send_user "failed to connect - timeout\n"
+	exit
+} eof {
+	send_user "failed to connect\n"
+	exit
+} "login*" { 
+	send -s "$user\r" 
+  exp_continue
+} "assword:*" { 
+	send -s "$pass\r" 
+  exp_continue
+} "*-> " { 
+	send -s "\r" 
+
+}
+
+set time     [ timestamp -format "%Y/%m/%d %H:%M:%S"]
+set filetime [ timestamp -format "%Y%m%d_%H%M%S"]
+
+;#send_user "\nLogin time is $time\n"
+
+;# xterm title change
+send_user "\033]2;$name $filetime\007"
+
+set fp [open "/etc/hosts" r]
+while { [gets $fp line] >=0 } {
+	if {[regexp $name $line]} {
+		set line [split $line "\t"]
+		set ip [lindex $line 0]
+	}
+}
+close $fp
+send_user "\n$name IP is $ip\n"
+
+
+log_file "/home/case/store/work/_archives_worklogs/$name-$filetime.log"
+
+send_log "\n---------- log start at $time ----------\n"
+
+interact {
+	;# so that we don't get logout when idle
+	;# timeout 180 { send " \b"}
+	\004 {
+		send_user "\nbye, bye\n"
+		set pid [exp_pid]
+		system "kill $pid"
+	}
+
+	\001l {
+		send -s "\r"		
+		expect  "login*" {
+			send -s "$user\r"
+		  exp_continue
+    } "assword:*" {
+			send -s "$pass\r"
+      exp_continue
+		} "*-> " {
+			send -s "\r"
+		}
+
+	}
+	\001i {
+		send_user "\n$name IP is $ip\n"
+	}
+	\001c {
+		set curtime [ timestamp -format "%m/%d/%Y %H:%M:%S"]
+		send -s "set clock $curtime\r"
+		expect "$prompt"
+		send -s "set hostname lab-$name\r"
+		expect "$prompt"
+		send -s "set interface mgt ip $ip/23\r"
+		expect "$prompt"
+		send -s "set interface eth0 ip $ip/23\r"
+		expect "$prompt"
+		send -s "set interface eth0/0 ip $ip/23\r"
+		expect "$prompt"
+		send -s "set interface eth0 manage\r"
+		expect "$prompt"
+		send -s "set interface eth0/0 manage\r"
+		expect "$prompt"
+		foreach cmd {	"set console page 0"
+				"set console timeout 0"
+				"set dbuf size 4096"
+				"set admin name netscreen"
+				"set admin password netscreen"
+				"set admin auth server \"Local\""} {
+			send -s "$cmd\r"
+			expect "$prompt"
+		}
+		foreach alias {	"ss \"save software from tftp 172.30.73.133\""
+				"sc \"save config from tftp 172.30.73.133\""
+        "sh \"get\""
+				"wr \"save\""
+				"reload \"reset no-prompt\""} {
+			send -s "set alias $alias\r"
+			expect "$prompt"
+		}
+	}
+;#++	\0010 {
+;#++		set timeout 3600
+;#++		match_max 10240000
+;#++		send -s "get tech-support\r"
+;#++		expect "$prompt"
+;#++		send -s "get event\r"
+;#++		expect "$prompt"		
+;#++		send -s "get log sys\r"
+;#++		expect "$prompt"		
+;#++		send -s "get nsrp\r"
+;#++		expect "$prompt" 
+;#++		send -s "get nsrp monitor\r"
+;#++		expect "$prompt" 
+;#++		send -s "get interface\r"
+;#++
+;#++		for { set x 1 } { $x<=4 } { incr x } {
+;#++			
+;#++			expect "$prompt"
+;#++			send -s "get nsrp\r"
+;#++			expect "$prompt"
+;#++			send -s "get nsrp counter\r"
+;#++			expect "$prompt"
+;#++			send -s "get nsrp coun packet\r"
+;#++			expect "$prompt"
+;#++			send -s "get interface\r"
+;#++		}
+;#++
+;#++
+;#++		expect "$prompt"
+;#++		send -s "get db stream\r"
+;#++		expect "$prompt"
+;#++		sleep 5
+;#++		match_max -d
+;#++		set timeout 30
+;#++	}
+	\001t {
+		set timeout 3600
+		set prompt "*-> "
+		for { set x 1 } { $x<=130 } { incr x } {
+			send -s "set route 10.0.$x.0/24 interface eth0/0 gateway 172.30.72.1\r"
+			expect "$prompt"
+			sleep 1 
+		}
+		send_user "\ndone\n"
+		send -s "\r"
+		set timeout 30
+	}
+
+
+	\001g {
+		set timeout 3600
+		set prompt "*-> "
+		for { set x 1 } { $x<=2 } { incr x } {
+			send -s "get clock\r"
+			expect "$prompt"
+			sleep 10
+		}
+		send_user "\ndone\n"
+		send -s "\r"
+		set timeout 30
+	}
+
+
+	\177 {
+		send "\010"
+	}
+	"\033\[3~" {
+		send "\177"
+	}
+}
+
+set time     [ timestamp -format "%Y/%m/%d %H:%M:%S"]
+send_log "\n---------- log close at $time ----------\n"
+log_file
+exec /bin/bzip2 /home/case/store/work/_archives_worklogs/$name-$filetime.log
+exit
--- expect/nsm_login.ex
+++ expect/nsm_login.ex
@@ -1,1 +1,378 @@
-./autologin/nsm_login.ex
+#!/usr/bin/expect -f
+
+# $Id$
+
+#### TO DO:
+#
+# - auto installation of NSM possible from inside this client,
+# - auto log monitoring and doing an action once a pattern is found,
+
+set logdir "/home/case/store/work/_archives_worklogs/"
+
+set send_slow {10 .01}
+set timeout 60
+set app nsm
+set os "Linux"
+
+if { $argc == 0 } {
+  puts "
+Usage: $argv0 <username@>host <password>
+
+If not specified username defaults to \"root\" and password to \"netscreen\".
+
+The connection to target host is done with normal ssh command without any arguments. 
+My suggestion is to use ~/.ssh/config for any non standard options.
+" 
+  exit
+} else {
+  set host [lindex $argv 0]
+  set user "root"
+  set pass "netscreen"
+  if { [regexp @ $host] } {
+    regexp {(\S+?)@} $host match user
+    regexp {@(\S+)} $host match host
+  }
+  if { $argc > 1 } {
+    set pass [lindex $argv 1]
+  }
+}
+
+spawn ssh $user@$host
+
+set stime     [ timestamp -format "%Y/%m/%d %H:%M:%S"]
+set filetime [ timestamp -format "%Y%m%d_%H%M%S"]
+
+puts "\033]0;$host $filetime\007"
+
+expect timeout {
+  send_user "connection timeout\n"
+  exit
+} eof {
+  send_user "got eof\n"
+  exit
+} "Permission denied, please try again." {
+  send_user "wrong credentials\n"
+  exit
+} "Are you sure you want to continue connecting (yes/no)?" {
+  send -s "yes\r"
+  exp_continue
+} "assword:" {
+  send -s "$pass\r"
+  exp_continue
+} "Run NSMXPress system setup" {
+  set app nsm 
+  send -s "n"
+  exp_continue
+} "1-6,QR" {
+  set app space
+  send -s "6"
+  exp_continue
+} "1-7,QR" {
+  set app space
+  send -s "7"
+  exp_continue
+} "*$ " {
+  send -s "sudo su -\r"
+  exp_continue
+} "*# " {
+  send "echo \${SHELL}\r"
+}
+
+
+### make sure we are running bash
+expect "*#" {
+  if { ! [regexp "bash" $expect_out(buffer)] } {
+    send -s "bash\r"
+  } else {
+    send "\r"
+  }
+}
+
+expect "*#" {
+#  send -s "set -o vi;export HISTCONTROL=ignoredups;export PS1='\\a\[\\D{%Y-%m-%d %H:%M:%S}\]\\n\\u@\\h:\\w \\\$ ';export PROMPT_COMMAND='echo -ne \"\\a\\033_\${USER}@\${HOSTNAME%%.*}:\${PWD}\\033\\\\\"';uname\r"
+  send -s "set -o vi;export HISTCONTROL=ignoredups;export PS1='\\a\[\\D{%Y-%m-%d %H:%M:%S}\]\\n\\u@\\h:\\w \\\$ ';uname\r"
+}
+
+expect "*#" {
+  if { [regexp "Linux" $expect_out(buffer)] } {
+    set os "Linux"
+  } elseif { [regexp "SunOS" $expect_out(buffer)] } {
+    set os "SunOS"
+  } else {
+    return
+  }
+  send "\r"
+}
+
+expect "*#" {
+  if { $os == "Linux" } {
+    if { $app == "nsm" } {
+      send -s "unset TMOUT;ln -sf /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime;ntpdate -u 172.30.73.133;hwclock --systohc\r"
+    }
+  } elseif { $os eq "SunOS" } {
+    send "ntpdate -u 172.30.73.133\r"
+  } else {
+    send "\r"
+  }
+}
+
+
+if { $app == "nsm" } {
+  expect "*#" {
+    if { $os == "Linux" } {
+      send -s "ifconfig | perl -nle'/dr:(172.30.\\S+)/&&print\$1'\r"
+    } elseif { $os == "SunOS" } {
+      send -s "ifconfig -a | perl -nle'/net (172.30.\\S+)/&&print\$1'\r"
+    }
+  }
+
+  expect "*#" {
+    set ourip [regexp -inline -- {172\.30\.\d+\.\d+} $expect_out(buffer)]
+    send "\r"
+  }
+} else {
+  set ourip 127.0.0.1
+  send "\r"
+}
+
+if { $app == "nsm" } {
+  expect "*#" {
+    send "DBXML_DIR=`ls -1 -t /usr/netscreen/GuiSvr/utils|grep dbxml| head -n 1`;LD_LIBRARY_PATH=/usr/netscreen/GuiSvr/utils/\$DBXML_DIR/lib:\$LD_LIBRARY_PATH;export LD_LIBRARY_PATH\r"
+  }
+}
+
+expect "*# " {
+  log_file "$logdir/$host-$filetime.log"
+  send_log "\n---------- log start at $stime ----------\n"
+
+  send_user "
+--- Help key ---
+Ctrl+a h - all shortcuts\n"
+
+  send "\r"
+
+
+  interact {
+
+    \003  { send "\003" }
+    \004  { send "exit\r" 
+      set timeout 10
+      if { $app == "nsm" } { 
+        expect timeout {
+          set timeout 60
+        } eof {
+          return
+        } "*# " {
+          send "exit\r"
+          exp_continue
+        } "*$ " { 
+          send "exit\r" 
+          sleep 1
+          return
+        }
+      } 
+      if { $app == "space" } {
+        expect timeout {
+          set timeout 60
+        } ",QR" {
+          send "q"
+          sleep 1
+          return
+        }
+      }
+    }
+
+    \001h { 
+      send_user "
+--- Help ---
+
+IP that will be used during the installation is: $ourip
+
+Ctrl+a h - this message,
+Ctrl+a c - menu choice, including removal, cleanup, db changes and corrections,  
+#Ctrl+a d - download and do a clean install of a given NSM version,
+Ctrl+a i - can be entered during the nsm installation will answer all the questions (clean install Gui+Dev if installing from scratch or just refresh otherwise),
+Ctrl+a x - stop/status/start/restart/version on all three services,
+Ctrl+a t - prints current timestamp in the format %Y%m%d%H%M%S sutaible for naming backups/copies of files,
+#Ctrl+a f - trigger an action based on a pattern match, if pattern is found, a script on the remote machine will be run /root/data.sh
+"
+      send "\r"
+    }
+
+    \001x { 
+      send_user "\nType the action (stop/status/start/restart):"
+      stty cooked echo 
+      expect_user -re "(.*)\n"
+      stty raw -echo
+      set action $expect_out(1,string)
+      send -s "/etc/init.d/guiSvr $action; /etc/init.d/devSvr $action; /etc/init.d/haSvr $action\n"
+    }
+    
+    \001t {
+      set backuptime [ timestamp -format "%Y%m%d_%H%M%S"]
+      send -s "$backuptime"
+    }
+
+    \001c {
+      send_user "\nType the action number:
+ 1 - correct /usr/netscreen/DevSvr/var/devSvr.cfg by removing unneeded white characters (make a copy) and restart DevSvr,
+ 2 - truncate the schema,
+ 3 - correct the customer db (super password, IPs),
+ 4 - prepare a command that will uninstall all NSM packages and remove all the NSM data from the server, the command is printed without \\r at the end,
+
+ input: "
+      stty cooked echo 
+      expect_user -re "(.*)\n"
+      stty raw -echo
+      set action $expect_out(1,string)
+
+      # devsvr.cfg corrections of whitespaces
+      if { $action == 1 } {
+      
+        set backuptime [ timestamp -format "%Y%m%d_%H%M%S"]
+        send "perl -pi\".$backuptime\" -e 's/  +/ /g' /usr/netscreen/DevSvr/var/devSvr.cfg && /etc/init.d/devSvr restart\r" 
+     
+      # truncate schema
+      } elseif { $action == 2 } {
+        send "/etc/init.d/haSvr stop\r"
+        expect "*# " { send "/etc/init.d/guiSvr stop\r"}
+        expect "*# " { send "/etc/init.d/devSvr stop\r"}
+        #expect "*# " { send "mv -f /usr/netscreen/GuiSvr/var/dmi-schema-stage /usr/netscreen/GuiSvr/var/dmi-schema-stage.old\r"}
+        expect "*# " { send "rm -rf /usr/netscreen/GuiSvr/var/dmi-schema-stage\r"}
+        expect "*# " { send "cp --reply=yes -fpr /usr/netscreen/GuiSvr/lib/initVar/dmi-schema-stage /usr/netscreen/GuiSvr/var/dmi-schema-stage\r"}
+        expect "*# " { send "rm -f /usr/netscreen/GuiSvr/var/xdb/init/*\r"}
+        expect "*# " { send "rm -rf /tmp/Schemas*\r"}
+        expect "*# " { send "rm -rf /usr/netscreen/GuiSvr/var/Schemas-GDH/*\r"}
+        expect "*# " { send "sh /usr/netscreen/GuiSvr/utils/.truncateSchemaTables.sh /usr/netscreen/GuiSvr/var/xdb\r"}
+        expect "*# " { send "cp --reply=yes -fpr /usr/netscreen/GuiSvr/lib/initVar/xdb/init/* /usr/netscreen/GuiSvr/var/xdb/init/\r"}
+        expect "*# " { send "/etc/init.d/haSvr start\r"}
+        expect "*# " { send "/etc/init.d/guiSvr start\r"}
+        expect "*# " { send "/etc/init.d/devSvr start\r"}
+
+      # correct the customer db (super password, IPs)
+      } elseif { $action == 3 } {
+        send "/etc/init.d/haSvr stop\r"
+        expect "*# " { send "/etc/init.d/guiSvr stop\r"}
+        expect "*# " { send "/usr/netscreen/GuiSvr/utils/setperms.sh GuiSvr > /dev/null\r"}
+        expect "*# " { send "/bin/chmod +s /usr/netscreen/GuiSvr/utils/.installIdTool\r"}
+        expect "*# " { send "/usr/netscreen/GuiSvr/utils/.xdbUpdate.sh /usr/netscreen/GuiSvr/var/xdb admin 1 0 /__/password \"glee/aW9bOYEewkD/6Ri8sHh2mU=\" > /dev/null\r"}
+        sleep 1
+        expect "*# " { send "/usr/netscreen/GuiSvr/utils/.xdbUpdate.sh /usr/netscreen/GuiSvr/var/xdb server 0 0 /__/ip \"$ourip\" > /dev/null\r"}
+        sleep 1
+        expect "*# " { send "/usr/netscreen/GuiSvr/utils/.xdbUpdate.sh /usr/netscreen/GuiSvr/var/xdb server 0 1 /__/ip \"$ourip\" > /dev/null\r"}
+        sleep 1
+        expect "*# " { send "/usr/netscreen/GuiSvr/utils/.xdbUpdate.sh /usr/netscreen/GuiSvr/var/xdb shadow_server 0 1 /__/clientOneTimePassword \"dk2003ns\" > /dev/null\r"} 
+        sleep 1
+        set backuptime [ timestamp -format "%Y%m%d_%H%M%S"]
+        expect "*# " { send "perl -npi\".old_$backuptime\" -e 's/ +/ /g;s/(clientOneTimePassword\\s+)\\S*/\$1 dk2003ns/i;s/(default.printLevel\\s+)\\S*/\$1 debug/i;s/(ourRsaPrivateKey|theirRsaPublicKey)/#\$1/i;s/(guiSvrDirectiveHandler.max.heap|devSvrDirectiveHandler.max.heap\\s+)\\d+/\$1 1536000000/i' /var/netscreen/*Svr/*Svr.cfg > /dev/null\r"}
+        expect "*# " { send "/etc/init.d/haSvr restart\r"}
+        expect "*# " { send "/etc/init.d/guiSvr restart\r"}
+        expect "*# " { send "/etc/init.d/devSvr restart\r"}
+
+      # NSM removal
+      } elseif { $action == 4 } {
+        if { $os == "Linux" } {
+          send -s "rpm -qa | grep netscreen | xargs -r rpm -e ; rm -rf /var/netscreen/*/* /usr/netscreen/*" 
+        } elseif { $os == "SunOS" } {
+          send -s "pkgrm -n `pkginfo -c application | grep -i netscreen | grep -v NSCNpostgres | awk '{print \$2}' | xargs` &&  rm -rf /var/netscreen/ /usr/netscreen/"
+        } 
+
+      # default - unknown choice
+      } else {
+        send_user "\nUnknown choice\n"
+      }
+    }
+
+#    \001d {
+#      send_user "\nPlease enter the NSM version in LGB format: "
+#      stty cooked echo 
+#      expect_user -re "(.*)\n"
+#      stty raw -echo
+#      set nextnsmver $expect_out(1,string)
+#      system "date;uptime"
+#      send_user "|localy run: nsm_auto_install.pl $nextnsmver $ourip|\n"
+#    }
+
+#    \001f {
+#      
+#    }
+
+    \001i { 
+      send  "\r"
+      expect timeout {
+        send_user " "
+      } "Do you want to do NSM installation with base license? (y/n) *>" {
+        send "y\r" 
+        exp_continue
+      } "Enter selection (1-2)*>" {
+        send "2\r"
+        exp_continue
+      } "Will server(s) need to be reconfigured during the refresh? (y/n) *>" {
+        send "n\r"
+        exp_continue 
+      } "Enter selection (1-3)*>" {
+        send "3\r" 
+        exp_continue
+      } "Enter base directory location for management servers " {
+        send "\r"
+        exp_continue 
+      } "Enable FIPS Support? (y/n)*>" {
+        send "n\r"
+        exp_continue
+      } "Will this machine participate in an HA cluster? (y/n) *>" {
+        send "n\r"
+        exp_continue 
+      } "Enter database log directory location *>" {
+        send "\r"
+        exp_continue
+      } "Enter the management IP address of this server *>" {
+        send "$ourip\r"
+        exp_continue
+      } "Enter the https port for NBI service *>" {
+        send "\r"
+        exp_continue
+      } "Enter the https port for web server *>" {
+        send "\r"
+        exp_continue
+      } "Enter password (password will not display as you type)>" {
+        send "$pass\r"
+        exp_continue
+      } "Will a Statistical Report Server be used with this GUI Server? (y/n) *>" {
+        send "n\r"
+        exp_continue
+      } "UNIX password: " {
+        send "$pass\r"
+        exp_continue
+      } "Will server processes need to be restarted automatically in case of a failure? (y/n) *>" {
+        send "n\r"
+        exp_continue
+      } "Will this machine require local database backups? (y/n) *>" {
+        send "n\r"
+        exp_continue
+      } "Enter Postgres DevSvr Db port *> " {
+        send "\r"
+        exp_continue
+      } "Enter Postgres DevSvr Db super user *> " {
+        send "\r"
+        exp_continue
+      } "Start server(s) when finished? (y/n) *> " {
+        send "y\r"
+        exp_continue
+      } "Are the above actions correct? (y/n)> " {
+        send "y\r"
+        exp_continue
+      } "Specify location of PostgreSQL * bin *> " {
+        send "\r"
+        exp_continue
+      }
+    }
+  }
+}
+
+set time     [ timestamp -format "%Y/%m/%d %H:%M:%S"]
+send_log "\n---------- log close at $time ----------\n"
+log_file
+exec /bin/bzip2 $logdir/$host-$filetime.log
+send_user "\n\[+\] session lasted from $stime to $time\n\[+\] logfile: $logdir/$host-$filetime.log.bz2\n" 
+exit
+

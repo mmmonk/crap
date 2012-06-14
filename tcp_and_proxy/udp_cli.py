5ea94f089f4c75f1476310f11d96c432c3ef927b
Marek Lukaszuk <m.lukaszuk@gmail.com>
2012-06-14 17:10:42 +0200

    tcp_and_proxy/udp_cli.py and tcp_and_proxy/udp_srv.py - added sequence numbers to the proxy

--- tcp_and_proxy/udp_cli.py
+++ tcp_and_proxy/udp_cli.py
@@ -6,6 +6,7 @@ import sys
 from fcntl import fcntl, F_SETFL
 from os import O_NONBLOCK
 import time
+import struct
 
 def dtime(lt,dt):
   ct = time.time()
@@ -14,37 +15,74 @@ def dtime(lt,dt):
   else:
     return False
 
-UDP_IP="127.0.0.1"
-UDP_PORT=5005
+def header(seq,ack):
+  return struct.pack("BB",seq,ack)
 
+def incseq(seq):
+  seq += 1
+  if seq > 255:
+    return 0
+  return seq
+
+def debug(msg):
+  sys.stderr.write(msg+"\n")
+
+IP="127.0.0.1"
+PORT=5005
+
+maxlen = 1024 # <<- FIXME why this is 2 higher then on the server????
+seq = 0
+ack = 0
+rtt = 1
+snt = 1
+notyet = 0
+
+dstaddr = (IP,PORT)
 sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
 sock.setblocking(0)
 
 fcntl(0, F_SETFL, O_NONBLOCK)
 
-sock.sendto("",(UDP_IP,UDP_PORT))
+sock.sendto(header(seq,ack),dstaddr)
 
 lt = time.time()
 
 while True:
-  toread,[],[] = select([0],[],[],1)
-  [],towrite,[] = select([],[1],[],1)
+  toread,towrite,[] = select([0],[1],[],10)
 
   if 1 in towrite:
     try:
-      data, addr = sock.recvfrom ( 1024 )
+      data, addr = sock.recvfrom (maxlen)
     except socket.error:
+      select([],[],[],0.2)
       pass
     else:
-      sys.stdout.write(data)
+      if addr == dstaddr:
+        head = struct.unpack("BB",data[:2])
+        if seq == head[1]:
+          if len(data[2:]) > 0:
+            sys.stdout.write(data[2:])
+          ack = head[0]
+          seq = incseq(seq)
+          #rtt = time.time() - snt
+          notyet = 0
+        else:
+          sys.stderr.write("[!] wrong seq\n")
+      else:
+        sys.stderr.write("[!] wrong source address: "+str(addr)+"\n")
 
-  if 0 in toread:
-    si = sys.stdin.read(1024)
+  if 0 in toread and notyet == 0:
+    si = sys.stdin.read(maxlen)
     if len(si) == 0:
       sys.exit()
+    
     else:
-      sock.sendto(si,(UDP_IP,UDP_PORT))
+      sock.sendto(header(seq,ack)+si,dstaddr)
+      notyet = 1
       lt = time.time()
-  
-  if dtime(lt,0.2):
-    sock.sendto("",(UDP_IP,UDP_PORT))
+      snt = lt
+
+  if dtime(lt,0.1) and notyet == 0:
+    sock.sendto(header(seq,ack),dstaddr)
+    notyet = 1
+    snt = time.time()
--- tcp_and_proxy/udp_srv.py
+++ tcp_and_proxy/udp_srv.py
@@ -4,15 +4,33 @@ import socket
 import time
 from select import select
 import sys
+import struct
 
-maxlen = 1024
+def header(seq,ack):
+  return struct.pack("BB",seq,ack)
 
-UDP_IP="127.0.0.1"
-UDP_PORT=5005
+def incseq(seq):
+  seq += 1
+  if seq > 255:
+    return 0
+  return seq
+
+def debug(msg):
+  sys.stderr.write(msg+"\n")
+
+maxlen = 1022
+
+IP="127.0.0.1"
+PORT=5005
 
 sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-sock.bind( (UDP_IP,UDP_PORT) )
+sock.bind((IP,PORT))
 sock.setblocking(0)
+
+seq = 0
+ack = 0
+rtt = 1
+snt = 1
 
 caddr = ("",0)
 
@@ -20,6 +38,7 @@ while True:
   try:
     data, addr = sock.recvfrom( maxlen )
   except socket.error:
+    select([],[],[],0.1)
     pass
   else:
     if not addr == caddr: 
@@ -28,19 +47,30 @@ while True:
       except NameError:
         pass
       serv = socket.socket( socket.AF_INET, socket.SOCK_STREAM ) 
-      serv.connect( (UDP_IP,22) )
+      serv.connect( (IP,22) )
+      head = struct.unpack("BB",data[:2])
+      seq = head[1]
 
     caddr = addr
+    head = struct.unpack("BB",data[:2])
 
-    toread,[],[] = select([serv],[],[],1)
-    [],towrite,[] = select([],[serv],[],1)
-    if serv in towrite:
-      serv.send(data)
-    if serv in toread:
-      servdata = serv.recv(maxlen)
-      if len(servdata) == 0:
-        serv.shutdown(socket.SHUT_RDWR)
-        sys.exit()
-      else:
-        sock.sendto(servdata,addr)
+    if seq == head[1]:
+      ack = head[0]
+      seq = incseq(seq) 
+      send = 0
+      toread,towrite,[] = select([serv],[serv],[],10)
+      if serv in towrite and len(data[2:])>0:
+        serv.send(data[2:])
+      if serv in toread:
+        servdata = serv.recv(maxlen)
+        if len(servdata) == 0:
+          serv.shutdown(socket.SHUT_RDWR)
+          sys.exit()
+        else:
+          sock.sendto(header(seq,ack)+servdata,addr)
+          send = 1
 
+      if send == 0:
+        sock.sendto(header(seq,ack),addr)
+    else:
+      sys.stderr.write("[!] wrong seq\n")

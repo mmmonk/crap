7bf202714b9fa7f712435e6528c6fbe229d847c9
Marek Lukaszuk <m.lukaszuk@gmail.com>
2012-06-27 19:44:53 +0200

    crypto/sss.py - added lots of comments

--- crypto/sss.py
+++ crypto/sss.py
@@ -11,7 +11,9 @@ getcontext().prec = 1000
 
 # https://en.wikipedia.org/wiki/Lagrange_polynomial
 # this is a special case in which the Lagrange L(x) 
-# function is actually L(0)
+# function is actually L(0), ie:
+# given polynomial a+bx+bx^2=y we "hide" the secret as
+# value "a", so knowing enough points what we do is basically f(0) 
 
 def Lagrange_polynomial(points):
   total = Decimal(0)
@@ -29,14 +31,16 @@ def Lagrange_polynomial(points):
 
 def usage():
   print sys.argv[0]+" <options> \n\n\
-      Usage:\n\
-      -h      - this screen,\n\
-      -e msg  - generate secrets for msg,\n\
-      -d      - recover secret based on the data in file (-f),\n\
-      -f file - file to either write secrets or read them,\n\
-      -a num  - overall number of secrets,\n\
-      -r num  - required number of secrets,\n\
-      "
+  Usage:\n\n\
+  This program implements in a very simple and basic way\n\
+  Shamir's secret-sharing scheme:\n\
+  https://en.wikipedia.org/wiki/Shamir%27s_Secret_Sharing\n\n\
+  -h      - this screen,\n\
+  -e msg  - generate secrets for msg,\n\
+  -d      - recover secret based on the data in file (-f),\n\
+  -f file - file to either write secrets or read them,\n\
+  -a num  - overall number of secrets (not less then -r),\n\
+  -r num  - required number of secrets (min 2),\n"
 
 if __name__ == "__main__":
 
@@ -49,6 +53,7 @@ if __name__ == "__main__":
   opt_all = 0
   opt_req = 0
 
+  # arguments processing
   try:
     while 1:
       
@@ -57,26 +62,26 @@ if __name__ == "__main__":
 
       arg = sys.argv[i]
 
-      if arg == "-h":
+      if arg == "-h": # help/usage 
         usage()
-      elif arg == "-e":
+      elif arg == "-e": # encode
         i += 1
         if i >= maxargv:
           sys.exit(1)
         opt_enc = sys.argv[i]+" "
-      elif arg == "-d":
+      elif arg == "-d": # decode
         opt_dec = 1
-      elif arg == "-f":
+      elif arg == "-f": # filename 
         i += 1
         if i >= maxargv:
           sys.exit(1)
         opt_fd = sys.argv[i]
-      elif arg == "-a":
+      elif arg == "-a": # all shares 
         i += 1
         if i >= maxargv:
           sys.exit(1)
         opt_all = int(sys.argv[i])
-      elif arg == "-r":
+      elif arg == "-r": # required shares
         i += 1
         if i >= maxargv:
           sys.exit(1)
@@ -88,15 +93,20 @@ if __name__ == "__main__":
     usage()
     sys.exit(1)
 
-  if not opt_enc == "":
+  if not opt_enc == "" and opt_req > 1: # we are encoding it here
+
+    if opt_all < opt_req:
+      opt_all = opt_req
 
     xv = []
     a = int(opt_enc.encode('hex'),16)
-    xv.append(a)
+    xv.append(a) # this is our secret, value a*x^0
     
+    # randomly generating all other 
     for i in xrange(opt_req-1):
       xv.append(random.randint(-1*a,a))
 
+    # randomly pick x to calculate f(x)
     xseen = {}
     for i in xrange(1,opt_all+1):
       while 1:
@@ -106,11 +116,13 @@ if __name__ == "__main__":
             pass
         except KeyError:
           break
-      
+     
+      # counting f(x)=y
       y = xv[0]
       for j in xrange(1,len(xv)):
         y += xv[j]*(x**j)
 
+      # text output, convert y to base32 
       line = str(x)+":"+b32encode(str(y))
       if not opt_fd == "":
         try:
@@ -121,18 +133,23 @@ if __name__ == "__main__":
       else:
         print line
      
-  elif opt_dec == 1 and not opt_fd == "":
+  elif opt_dec == 1 and not opt_fd == "": # decoding part
     try:
+      # reading from a file
+      # format:
+      # x:base32(y)
       lines = [ line.strip().split(":") for line in open(opt_fd).readlines()]
     except IOError:
       print "file "+str(opt_fd)+" could not be read !"
       usage()
       sys.exit(1)
-    
+   
+    # converting text to tuples (x,y) 
     points = [ (int(x),int(b32decode(y))) for x,y in lines]
-
+    
+    # coming up with the secret value
     out = hex(int(Lagrange_polynomial(points))).replace("0x","").replace("L","")
     print out.decode('hex')
 
-  else:
+  else: # something went wrong
     usage()

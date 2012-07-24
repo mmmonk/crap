947d4fa0f279fff9f336c33e004b20ba0bd7f829
Marek Lukaszuk <m.lukaszuk@gmail.com>
2012-07-24 16:23:44 +0200

    cjc.pl - print just one empty line instead of multiple

--- jnpr/screenos/cjc.pl
+++ jnpr/screenos/cjc.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 # $Id: 20120722$
-# $Date: 2012-07-22 15:09:34$
+# $Date: 2012-07-22 17:37:43$
 # $Author: Marek Lukaszuk$
 
 # clearing a bit the output of "get tech"
@@ -10,8 +10,16 @@ use strict;
 use warnings;
 use integer;
 
+my $empty = 0;
+
 while(<>){
-  next if /^\s*$/;
+  if (/^\s*$/){
+    $empty++;
+  }else{
+    $empty = 0;
+  }
+  next if ($empty > 1);
+  s/(\x07)+/\n/g;
   s/--- more ---              //;
   s/---\(more\)---(\x0d\s+\x0d)?//;
   s/---\(more \d+%\)---(\x0d\s+\x0d)?//;

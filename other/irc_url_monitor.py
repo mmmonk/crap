5fcedc2211d991c731190b6bb21bdbe5a57f60b8
Marek Lukaszuk <m.lukaszuk@gmail.com>
2012-09-25 20:27:19 +0200

    other/irc_url_monitor.py - small corrections

--- other/irc_url_monitor.py
+++ other/irc_url_monitor.py
@@ -1,42 +1,42 @@
 #!/usr/bin/env python
 
-import sys
-import os
-import time
-import smtplib
+import sys, os, time, smtplib
 
 urlfile = "/home/case/.irssi/url"
 urlhistory = "/home/case/.irssi/urlhistory"
 
 if __name__ == '__main__':
-  
+
   seenurls = {}
   newlinks = {}
 
-  cleanolderthenthis = 30*24*3600 
+  cleanolderthenthis = 60*24*3600
   keepnumlimit = 5000
 
-  oldlimit = int(time.time()) - cleanolderthenthis 
+  oldlimit = int(time.time()) - cleanolderthenthis
   if os.path.isfile(urlhistory) and os.stat(urlhistory).st_size > 0:
     for line in open(urlhistory,'r').readlines():
       (url,ts) = line.split()
       if ts > oldlimit:
-        seenurls[url] = ts 
+        seenurls[url] = ts
 
   if os.path.isfile(urlfile) and os.stat(urlfile).st_size > 0:
-    for line in open(urlfile,'r').readlines(): 
+    for line in open(urlfile,'r').readlines():
       line = line.strip()
       line = line.strip(",.")
       linea = line.split()
-      
+
+      if "://" not in linea[9]:
+        continue
+
       if not seenurls.has_key(linea[9]):
-        newlinks[linea[9]] = linea[8]
-      
+        newlinks[linea[9]] = linea[8]+" "+linea[7]
+
       seenurls[linea[9]] = int(time.time())
-    
+
     if len(newlinks) > 0:
-    
-      msg = "From: irssi@monkey.geeks.pl\nTo: m.lukaszuk@gmail.com\nSubject: irssi links from "+(time.strftime("%Y/%m/%d %H:%M:%S",time.localtime()))+"\n\n" 
+
+      msg = "From: irssi@monkey.geeks.pl\nTo: m.lukaszuk@gmail.com\nSubject: irssi links from "+(time.strftime("%Y/%m/%d %H:%M:%S",time.localtime()))+"\n\n"
       for link,channel in sorted(newlinks.items(), key=lambda x: x[0].replace("//www.","//",1).split(":")[1]):
         msg += link+" "+channel+"\n"
 
@@ -44,12 +44,12 @@ if __name__ == '__main__':
       smtpObj.sendmail('m.lukaszuk@gmail.com','m.lukaszuk@gmail.com',msg)
 
       fd = open(urlhistory,'w')
-      i = 0 
-      for link,ts in sorted(seenurls.items(), key=lambda x: x[1], reverse=True):
+      i = 0
+      for link,ts in sorted(seenurls.items(), key = lambda x: x[1], reverse=True):
         fd.write(link+" "+str(ts)+"\n")
         i += 1
         if i >= keepnumlimit:
           break
       fd.close()
-      
+
       os.unlink(urlfile)

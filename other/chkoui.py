c02320a3f2165dd2f2d37d42863dbc0f6814b644
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-02-27 22:17:17 +0100

    other/chkoui.py - added a check if the mac exists and two additional messages

--- other/chkoui.py
+++ other/chkoui.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # $Id: 20130227$
-# $Date: 2013-02-27 14:23:18$
+# $Date: 2013-02-27 22:15:05$
 # $Author: Marek Lukaszuk$
 
 import sys
@@ -15,19 +15,23 @@ ouidat = "/tmp/oui_cache.dat"
 def pon(oui,mac):
   # matching the mac to a vendor
   a ="".join(re.findall("[0-9a-f]",mac.lower()))[:6]
-  print str(a)+" - "+str(oui[a])
+  if oui.has_key(a):
+    print str(a)+" - "+str(oui[a])
+  else:
+    print str(a)+" - unknown"
 
 def refreshOUIdata(ouidat):
-  # download new OUI data from a link and pickle it 
+  # download new OUI data from a link and pickle it
   ouiurl = "http://standards.ieee.org/develop/regauth/oui/oui.txt"
-  
+
+  print "[+] downloading the oui database"
   oui = dict()
   for line in urllib2.urlopen(ouiurl).readlines():
     if '(hex)' in line:
       a = line.split()
       b = list()
       oui[a[0].replace("-","").lower()]=" ".join(a[2:])
-  
+
   fd = open(ouidat,"wb")
   p = pickle.Pickler(fd,protocol=2)
   p.dump(oui)
@@ -38,10 +42,10 @@ def loadOUI(ouidat):
   fd = open(ouidat,"rb")
   p = pickle.Unpickler(fd)
   return p.load()
-  
+
 oui = dict()
 
-if not os.path.isfile(ouidat): 
+if not os.path.isfile(ouidat):
   refreshOUIdata(ouidat)
 
 try:

57c7ad7284b8987e59094c14fbb13fe1d4242c1b
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-02-28 15:50:58 +0100

    tcp_and_proxy/nisroc.py - partial rewrite to a class version

--- tcp_and_proxy/nisroc.py
+++ tcp_and_proxy/nisroc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python -u
 
-# $Id: 20130227$
-# $Date: 2013-02-27 21:49:40$
+# $Id: 20130228$
+# $Date: 2013-02-28 14:48:13$
 # $Author: Marek Lukaszuk$
 
 from OpenSSL import SSL
@@ -9,84 +9,195 @@ import fcntl
 import os
 import select
 import sys
-from socket import socket,has_ipv6,AF_INET,AF_INET6,SOCK_STREAM,IPPROTO_TCP,TCP_CORK,error as socket_error
+import socket
 
-configfile = os.environ['HOME']+"/.nisrocrc"
-version = "$Rev$"
+version = "20130228"
 
-def loadconfig():
+class nisroc():
 
-  global hostdata
+  def __init__(self,configfile="~/.nisrocrc",host="",port=0 ,phost="",pport=8080 ,auth=""):
 
-  hostdata = {}
+    self.host = host
+    self.port = port
+    self.phost = phost
+    self.pport = pport
+    self.auth = auth
+    self.configfile = os.path.abspath(os.path.expanduser(configfile))
+    self.hostdata = dict()
 
-  try:
-    cfg = open(configfile,'r')
-  except:
-    sys.stderr.write("[-] config file not found - will not check digest\n")
-    return
+    try:
+      cfg = open(self.configfile,'r')
+    except:
+      self.sign("config file not found - will not check digest",bad=1)
+      sys.exit()
 
-  for line in cfg.readlines():
-    if line[0] != '#':
-      line = line.strip()
-      val = line.split(';')
-      sys.stderr.write("[?] data: "+str(val[0])+"-"+str(val[1])+"-"+str(val[2])+"-"+str(val[3])+"\n")
-      if len(val) == 4:
-        hostdata[str(val[0])+":"+str(val[1])] = str(val[2])+";"+str(val[3])
+    for line in cfg.readlines():
+      if line[0] != '#':
+        val = line.strip().split(';')
+        self.log("data: "+str(val[0])+"-"+str(val[1])+"-"+str(val[2])+"-"+str(val[3]))
+        if len(val) == 4:
+          hostdata[str(val[0])+":"+str(val[1])] = str(val[2])+";"+str(val[3])
 
-
-def appconfig(host,port,digest,key):
-
-  try:
-    cfg = open(configfile,'a')
-    cfg.write(str(host)+";"+str(port)+";"+str(digest)+";"+str(key)+"\n")
     cfg.close()
-  except:
-    sys.stderr.write("[-] error adding host to config file\n")
 
-# main data exchnage function
-def exchange(s):
+    # SSL context
+    self.ctx = SSL.Context(SSL.TLSv1_METHOD)
+    self.ctx.set_cipher_list('RC4')
 
-  # input:
-  # s - socket object
-  # c - second socket object
-  # return:
-  # nothing :)
+    if (socket.has_ipv6 == True and (":" in self.host or ":" in self.phost)):
+      self.conn = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    else:
+      self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_CORK,1)
 
-  # setting every descriptor to be non blocking
-  #fcntl(s, F_SETFL, O_NONBLOCK)
-  fcntl.fcntl(0, fcntl.F_SETFL, os.O_NONBLOCK)
+  def connect(self):
+    if not self.phost == "":
+      self.httpproxy()
+    else:
+      self.normalcon()
 
-  s_recv = s.recv
-  s_send = s.send
-  c_recv = sys.stdin.read
-  c_send = sys.stdout.write
+    try:
+      self.handshake()
+      self.exchange()
+      self.conn.shutdown()
+    except KeyboardInterrupt:
+      pass
 
-  while 1:
-    toread,[],[] = select.select([0,s],[],[],30)
-    [],towrite,[] = select.select([],[1,s],[],30)
 
-    if 1 in towrite and s in toread:
-      try:
-        data = s_recv(4096)
-      except SSL.WantReadError:
-        data = ''
-      except SSL.SysCallError:
-        s.sock_shutdown(0)
-        break
+  def appconfig(self,host,port,digest,key):
+    # adding new cert to the config
+    try:
+      open(self.configfile,'a').write(str(host)+";"+str(port)+";"+str(digest)+";"+str(key)+"\n")
+    except:
+      self.log("error adding host to config file",bad=1)
 
-      if len(data) > 0:
-        c_send(data)
+  def log(self,msg,bad=0):
+    # logging
+    if bad == 0:
+      sign = "+"
+    else:
+      sign = "-"
+    sys.stderr.write("["+str(sign)+"] "+str(msg)+"\n")
 
-    elif 0 in toread and s in towrite:
+  def exchange(self):
+    # main data exchange function
 
-      data = c_recv(4096)
+    # input:
+    # s - socket object
+    # c - second socket object
+    # return:
+    # nothing :)
 
-      if len(data) == 0:
-        s.sock_shutdown(0)
-        break
+    # no buffering for stdout
+    fcntl.fcntl(0, fcntl.F_SETFL, os.O_NONBLOCK)
+
+    while 1:
+      toread,[],[] = select.select([0,self.ssl],[],[],30)
+      [],towrite,[] = select.select([],[1,self.ssl],[],30)
+
+      if 1 in towrite and self.ssl in toread:
+        try:
+          data = self.ssl.recv(4096)
+        except SSL.WantReadError:
+          data = ''
+        except
+          break
+
+        if len(data) > 0:
+          sys.stdout.write(data)
+
+      elif 0 in toread and s in towrite:
+
+        data = sys.stdin.read(4096)
+
+        if len(data) == 0:
+          break
+        else:
+          self.ssl.send(data)
+
+    self.ssl.shutdown()
+
+
+  def httpproxy(self):
+
+    proxy_str = "CONNECT "+str(self.host)+":"+str(self.port)+" HTTP/1.0\r"
+
+    if not self.auth == ""
+      proxy_str = proxy_str + "Proxy-Authorization: Basic " + str(self.auth) + "\r"
+
+    try:
+      self.conn.connect((self.phost,self.pport))
+      self.conn.send(proxy_str+"\r")
+      data = self.conn.recv(128)
+      if "200 Connection established" in data:
+        pass
       else:
-        s_send(data)
+        self.conn.shutdown()
+        self.log("problem connecting to "+str(self.host)+":"+str(self.port)+" via proxy "+str(self.phost)+":"+str(self.pport)+" - maybe not allowed?",bad=1)
+        sys.exit()
+    except socket.error:
+      self.conn.shutdown()
+      sys.log("problem connecting to proxy "+str(self.phost)+":"+str(self.pport),bad=1)
+      sys.exit()
+
+
+  def normalcon(self):
+
+    try:
+      self.conn.connect((self.host,self.port))
+    except socket_error:
+      self.conn.shutdown()
+      self.log("problem connecting to "+str(self.host)+":"+str(self.port),bad=1)
+      sys.exit()
+
+
+  def handshake(self):
+
+    self.ssl = SSL.Connection(self.ctx,self.conn)
+    self.ssl.setblocking(True)
+    try:
+      self.ssl.set_connect_state()
+      self.ssl.do_handshake()
+    except:
+      self.log("ssl handshake error",bad=1)
+      sys.exit()
+
+    digest_save = 0
+    key = ""
+    if str(self.host)+":"+str(self.port) in hostdata:
+      digest_save,key = hostdata[str(self.host)+":"+str(self.port)].split(';')
+
+    digest = self.ssl.get_peer_certificate().digest('sha256')
+
+    if digest_save != 0:
+      if digest_save not in digest:
+        self.log("cert digest wrong, possible MITM, exiting",bad=1)
+        sys.exit()
+    else:
+      self.log("cert digest "+str(digest)+" - not verifed")
+
+    if key:
+      self.ssl.send(key)
+    elif 'nisroc' in os.environ:
+      self.ssl.send(os.environ['nisroc'])
+    else:
+      self.log("no key either in config file or in the env variable (nisroc) - exiting",bad=1)
+      sys.exit()
+
+    data = self.ssl.recv(1024)
+    if data and 'OpenSSH' in data:
+      self.log("connected succesfully - nisroc ("+str(version)+")")
+    else:
+      self.log("wrong key",bad=1)
+      sys.exit()
+
+    if key == 0:
+      self.log("adding host information to config file "+str(configfile))
+      appconfig(host,port,digest,os.environ['nisroc'])
+
+    sys.stdout.write(data)
+
+    self.ssl.setblocking(False)
 
 
 #### main stuff ####
@@ -104,94 +215,6 @@ if __name__ == '__main__':
     if len(sys.argv) >= 5:
       pport = int(sys.argv[4])
 
-    loadconfig()
-
-    ctx = SSL.Context(SSL.TLSv1_METHOD)
-
-    if (":" in host and has_ipv6 == True) or (len(sys.argv) >= 4 and ":" in phost and has_ipv6 == True):
-      proxy = socket(AF_INET6, SOCK_STREAM)
-    else:
-      proxy = socket(AF_INET, SOCK_STREAM)
-    proxy.setsockopt(IPPROTO_TCP, TCP_CORK,1)
-
-    if len(sys.argv) >= 4:
-
-      proxy_str = "CONNECT "+str(host)+":"+str(port)+" HTTP/1.0\r"
-
-      if len(sys.argv) >= 5:
-        proxy_str = proxy_str + "Proxy-Authorization: Basic " + str(sys.argv[5]) + "\r"
-
-      try:
-        proxy.connect((phost,pport))
-        proxy.send(proxy_str+"\r")
-        data = proxy.recv(128)
-        if "200 Connection established" in data:
-          pass
-        else:
-          proxy.close()
-          sys.exit("[-] problem connecting to "+str(host)+":"+str(port)+" via proxy "+str(phost)+":"+str(pport)+" - maybe not allowed?")
-
-      except socket_error:
-        proxy.close()
-        sys.exit("[-] problem connecting to proxy "+str(phost)+":"+str(pport))
-    else:
-      try:
-        proxy.connect((host,port))
-      except socket_error:
-        proxy.close()
-        sys.exit("[-] problem connecting to "+str(host)+":"+str(port))
-
-    ssl = SSL.Connection(ctx,proxy)
-    ssl.setblocking(True)
-    try:
-      ssl.set_connect_state()
-      ssl.do_handshake()
-    except:
-      sys.exit("[-] ssl handshake error")
-
-    digest_save = 0
-    key = 0
-    if str(host)+":"+str(port) in hostdata:
-      digest_save,key = hostdata[str(host)+":"+str(port)].split(';')
-
-    digest = ssl.get_peer_certificate().digest('sha256')
-
-    if digest_save != 0:
-      if digest_save not in digest:
-        sys.stderr.write("[-] cert digest wrong, possible MITM, exiting\n")
-    else:
-      sys.stderr.write("[?] cert digest "+str(digest)+" - not verifed\n")
-
-    if key:
-      ssl.send(key)
-    elif 'nisroc' in os.environ:
-      ssl.send(os.environ['nisroc'])
-    else:
-      sys.exit("[-] no key either in config file or in the env variable (nisroc) - exiting")
-
-    data = ssl.recv(1024)
-    if data and 'OpenSSH' in data:
-      sys.stderr.write("[+] connected succesfully - nisroc ("+str(version)+")\n")
-    else:
-      sys.exit("[-] wrong key")
-
-    if key == 0:
-      sys.stderr.write("[+] adding host information to config file "+str(configfile)+"\n")
-      appconfig(host,port,digest,os.environ['nisroc'])
-
-    stdout.write(data)
-
-    ssl.setblocking(False)
-
-    try:
-      exchange(ssl)
-    except KeyboardInterrupt:
-      pass
-
-    proxy.close()
-
   else:
     sys.stderr.write("usage: "+sys.argv[0]+" ip_dest port_dest <proxy_ip> <proxy_port> <base64 encoded string user:pass for proxy auth>\n")
-
-
 

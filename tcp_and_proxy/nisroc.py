e73e7ffdec7c6c94e3eb30864a772e3ee28d0503
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-03-01 13:46:34 +0100

    tcp_and_proxy/nisroc.py - added argparse support for arguments, fixed few small issues

--- tcp_and_proxy/nisroc.py
+++ tcp_and_proxy/nisroc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python -u
 
-# $Id: 20130228$
-# $Date: 2013-02-28 14:48:13$
+# $Id: 20130301$
+# $Date: 2013-03-01 13:46:17$
 # $Author: Marek Lukaszuk$
 
 from OpenSSL import SSL
@@ -10,35 +10,42 @@ import os
 import select
 import sys
 import socket
+import argparse
 
 version = "20130228"
 
 class nisroc():
 
-  def __init__(self,configfile="~/.nisrocrc",host="",port=0 ,phost="",pport=8080 ,auth=""):
+  def __init__(self,configfile="~/.nisrocrc",host="",port=0 ,phost="",pport=8080 ,auth="", dontcheckdigest=False, srvpass=""):
 
     self.host = host
     self.port = port
     self.phost = phost
     self.pport = pport
     self.auth = auth
+    self.srvpass = srvpass
     self.configfile = os.path.abspath(os.path.expanduser(configfile))
     self.hostdata = dict()
 
     try:
       cfg = open(self.configfile,'r')
     except:
-      self.sign("config file not found - will not check digest",bad=1)
-      sys.exit()
+      self.log("config file not found - will not check digest",bad=1)
+      if not dontcheckdigest:
+        sys.exit()
+      if self.srvpass == "":
+        sys.exit()
 
-    for line in cfg.readlines():
-      if line[0] != '#':
-        val = line.strip().split(';')
-        self.log("data: "+str(val[0])+"-"+str(val[1])+"-"+str(val[2])+"-"+str(val[3]))
-        if len(val) == 4:
-          hostdata[str(val[0])+":"+str(val[1])] = str(val[2])+";"+str(val[3])
 
-    cfg.close()
+    if os.path.isfile(self.configfile):
+      for line in cfg.readlines():
+        if line[0] != '#':
+          val = line.strip().split(';')
+          self.log("data: "+str(val[0])+"-"+str(val[1])+"-"+str(val[2])+"-"+str(val[3]))
+          if len(val) == 4:
+            hostdata[str(val[0])+":"+str(val[1])] = str(val[2])+";"+str(val[3])
+
+      cfg.close()
 
     # SSL context
     self.ctx = SSL.Context(SSL.TLSv1_METHOD)
@@ -59,7 +66,7 @@ class nisroc():
     try:
       self.handshake()
       self.exchange()
-      self.conn.shutdown()
+      self.conn.shutdown(2)
     except KeyboardInterrupt:
       pass
 
@@ -82,13 +89,7 @@ class nisroc():
   def exchange(self):
     # main data exchange function
 
-    # input:
-    # s - socket object
-    # c - second socket object
-    # return:
-    # nothing :)
-
-    # no buffering for stdout
+    # stdout non blocking
     fcntl.fcntl(0, fcntl.F_SETFL, os.O_NONBLOCK)
 
     while 1:
@@ -100,13 +101,13 @@ class nisroc():
           data = self.ssl.recv(4096)
         except SSL.WantReadError:
           data = ''
-        except
+        except:
           break
 
         if len(data) > 0:
           sys.stdout.write(data)
 
-      elif 0 in toread and s in towrite:
+      elif 0 in toread and self.ssl in towrite:
 
         data = sys.stdin.read(4096)
 
@@ -115,14 +116,13 @@ class nisroc():
         else:
           self.ssl.send(data)
 
-    self.ssl.shutdown()
-
+    self.ssl.shutdown(2)
 
   def httpproxy(self):
 
     proxy_str = "CONNECT "+str(self.host)+":"+str(self.port)+" HTTP/1.0\r"
 
-    if not self.auth == ""
+    if not self.auth == "":
       proxy_str = proxy_str + "Proxy-Authorization: Basic " + str(self.auth) + "\r"
 
     try:
@@ -132,11 +132,11 @@ class nisroc():
       if "200 Connection established" in data:
         pass
       else:
-        self.conn.shutdown()
+        self.conn.shutdown(2)
         self.log("problem connecting to "+str(self.host)+":"+str(self.port)+" via proxy "+str(self.phost)+":"+str(self.pport)+" - maybe not allowed?",bad=1)
         sys.exit()
     except socket.error:
-      self.conn.shutdown()
+      self.conn.shutdown(2)
       sys.log("problem connecting to proxy "+str(self.phost)+":"+str(self.pport),bad=1)
       sys.exit()
 
@@ -145,8 +145,7 @@ class nisroc():
 
     try:
       self.conn.connect((self.host,self.port))
-    except socket_error:
-      self.conn.shutdown()
+    except socket.error:
       self.log("problem connecting to "+str(self.host)+":"+str(self.port),bad=1)
       sys.exit()
 
@@ -162,24 +161,24 @@ class nisroc():
       self.log("ssl handshake error",bad=1)
       sys.exit()
 
-    digest_save = 0
+    digest_save = ""
     key = ""
-    if str(self.host)+":"+str(self.port) in hostdata:
-      digest_save,key = hostdata[str(self.host)+":"+str(self.port)].split(';')
+    if str(self.host)+":"+str(self.port) in self.hostdata:
+      digest_save,key = self.hostdata[str(self.host)+":"+str(self.port)].split(';')
 
-    digest = self.ssl.get_peer_certificate().digest('sha256')
+    digest = self.ssl.get_peer_certificate().digest('sha1')
 
-    if digest_save != 0:
-      if digest_save not in digest:
-        self.log("cert digest wrong, possible MITM, exiting",bad=1)
+    if digest_save != "":
+      if digest_save != digest:
+        self.log("cert digest wrong, possible MITM",bad=1)
         sys.exit()
     else:
       self.log("cert digest "+str(digest)+" - not verifed")
 
     if key:
       self.ssl.send(key)
-    elif 'nisroc' in os.environ:
-      self.ssl.send(os.environ['nisroc'])
+    elif not self.srvpass == "":
+      self.ssl.send(self.srvpass)
     else:
       self.log("no key either in config file or in the env variable (nisroc) - exiting",bad=1)
       sys.exit()
@@ -203,18 +202,16 @@ class nisroc():
 #### main stuff ####
 if __name__ == '__main__':
 
-  if len(sys.argv) >= 3:
+  p = argparse.ArgumentParser()
+  p.add_argument("host", help="destination host")
+  p.add_argument("port", type=int, help="destination port")
+  p.add_argument("-ph", default="", help="http proxy host")
+  p.add_argument("-pp", type=int, default=8080, help="http proxy port (default: 8080)")
+  p.add_argument("-auth", default="", help="base64 encoded string user:pass for proxy auth")
+  p.add_argument("-pwd", default="", help="the pass string to connect to the \"hidden\" service")
+  p.add_argument("-dc", action='store_true', help="don't check server digest with the local file (it will still be stored if it is not there)")
 
-    host = sys.argv[1]
-    port = int(sys.argv[2])
+  args = p.parse_args()
 
-    if len(sys.argv) >= 4:
-      phost = sys.argv[3]
-      pport = 8080
-
-    if len(sys.argv) >= 5:
-      pport = int(sys.argv[4])
-
-  else:
-    sys.stderr.write("usage: "+sys.argv[0]+" ip_dest port_dest <proxy_ip> <proxy_port> <base64 encoded string user:pass for proxy auth>\n")
-
+  c = nisroc(host=args.host, port=args.port, phost=args.ph, pport=args.pp, auth=args.auth, dontcheckdigest=args.dc, srvpass=args.pwd)
+  c.connect()

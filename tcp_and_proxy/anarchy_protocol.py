7c306284f3475112bc746f4f6c7b8a76bfac754d
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-03-01 17:34:14 +0100

    tcp_and_proxy/anarchy_protocol.py - added a bunch of TODOs

--- tcp_and_proxy/anarchy_protocol.py
+++ tcp_and_proxy/anarchy_protocol.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-# $Id: 20130227$
-# $Date: 2013-02-27 22:08:30$
+# $Id: 20130301$
+# $Date: 2013-03-01 16:43:25$
 # $Author: Marek Lukaszuk$
 
 # anarchy protocol aka stateless protocol ;)
@@ -11,9 +11,11 @@ import os
 import time
 import struct
 
+# TODO
 # protocol my somehow keep track of a session but it can't relay on any src addressesa
 # SYN "packets" should allow for exchange of DH (suffciently big - in the data part)
 # that will be used for xoring over the transfered data.
+# There should be a two byte session id, for each session.
 
 class anarchy():
 
@@ -25,8 +27,9 @@ class anarchy():
     self.seq = 0
     self.ack = 0
     self.sent = dict() # this will be used by the client to make sure it keeps track of send packets
-    self.headfmt = "BBBH"
+    self.headfmt = "BBBH" # TODO
     self.headsize = struct.calcsize(self.headerfmt)
+    self.sessid = 0
 
   def log(self, msg):
     # internal logging function
@@ -41,6 +44,7 @@ class anarchy():
     sys.stderr.write(txt+"\n")
 
   def dechead(self, head):
+    # TODO
     pass
 
   def enchead(self, syn=0, fin=0, keepalive=0, moredata=0):
@@ -49,7 +53,7 @@ class anarchy():
     # seq = 1 byte
     # ack = 1 byte
     # size = 2 bytes
-
+    # TODO
     return ""
 
   def decflags(self, flags):
@@ -63,13 +67,15 @@ class anarchy():
     return data
 
 
+# TODO a lot below
 class aserver(anarchy):
+  # server class
 
   def __init__(self, lserver="127.0.0.1", lport=22):
     self.lserver = lserver
     self.lport = lport
 
 class aclient(anarchy):
-
+  # server class
   def __init__(self):
     pass

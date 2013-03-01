576cf7c92cee36135480583f9c2df4eb6f4e0baa
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-03-01 10:13:03 +0100

    other/chkoui.py - rewrite, class with refreshing db

--- other/chkoui.py
+++ other/chkoui.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-# $Id: 20130228$
-# $Date: 2013-02-28 20:45:37$
+# $Id: 20130301$
+# $Date: 2013-03-01 10:13:00$
 # $Author: Marek Lukaszuk$
 
 import sys
@@ -10,54 +10,81 @@ import urllib2
 import re
 import pickle
 import bz2
+import time
 
-ouidat = "/tmp/oui_cache.dat"
+class checkOUI():
 
-def pon(oui,mac):
-  # matching the mac to a vendor
-  a ="".join(re.findall("[0-9a-f]",mac.lower()))[:6]
-  if oui.has_key(a):
-    print str(a)+" - "+str(oui[a])
+  def __init__(self, db="/tmp/oui_cache.dat"):
+    self.ouidat = os.path.dirname(db)+os.sep+os.environ['USER']+"_"+os.path.basename(db)
+    self.url = "http://standards.ieee.org/develop/regauth/oui/oui.txt"
+    self.oui = dict()
+
+    if not os.path.isfile(self.ouidat):
+      self.downloadDB()
+
+    # refresh the db after a month (30 days)
+    if int(time.time())-os.stat(self.ouidat)[8] > 2592000:
+      self.downloadDB(refresh=True)
+
+    try:
+      self.loadDB()
+    except:
+      self.downloadDB()
+      self.loadDB()
+
+
+  def check(self,mac):
+    # matching the mac to a vendor
+    a ="".join(re.findall("[0-9a-f]",mac.lower()))[:6]
+    if self.oui.has_key(a):
+      return str(a)+" - "+str(self.oui[a])
+    return str(a)+" - unknown"
+
+  def downloadDB(self, refresh=False):
+    # download new OUI data from a link and pickle it
+
+    print "[+] downloading the oui database"
+    oui = dict()
+    fail = False
+    try:
+      for line in urllib2.urlopen(self.url).readlines():
+        if '(hex)' in line:
+          a = line.split()
+          b = list()
+          oui[a[0].replace("-","").lower()]=" ".join(a[2:])
+    except:
+      print "[-] error while downloading the file"
+      fail = True
+      if refresh == False:
+        sys.exit()
+
+    if fail == False:
+      try:
+        fd = bz2.BZ2File(self.ouidat+"tmp","wb")
+        pickle.Pickler(fd,protocol=2).dump(oui)
+        fd.close()
+      except:
+        print "[-] error while saving file"
+        fail = True
+        if refresh == False:
+          sys.exit()
+
+    if fail == False:
+      os.rename(self.ouidat+"tmp",self.ouidat)
+
+  def loadDB(self):
+    # load pickled OUI data from file
+    fd = bz2.BZ2File(self.ouidat,"rb")
+    self.oui = pickle.Unpickler(fd).load()
+    fd.close()
+
+if __name__ == "__main__":
+
+  oui = checkOUI()
+
+  if len(sys.argv) == 1:
+    for mac in sys.stdin.readlines():
+      print oui.check(mac)
   else:
-    print str(a)+" - unknown"
-
-def refreshOUIdata(ouidat):
-  # download new OUI data from a link and pickle it
-  ouiurl = "http://standards.ieee.org/develop/regauth/oui/oui.txt"
-
-  print "[+] downloading the oui database"
-  oui = dict()
-  for line in urllib2.urlopen(ouiurl).readlines():
-    if '(hex)' in line:
-      a = line.split()
-      b = list()
-      oui[a[0].replace("-","").lower()]=" ".join(a[2:])
-
-  fd = bz2.BZ2File(ouidat,"wb")
-  p = pickle.Pickler(fd,protocol=2)
-  p.dump(oui)
-  fd.close()
-
-def loadOUI(ouidat):
-  # load pickled OUI data from file
-  fd = bz2.BZ2File(ouidat,"rb")
-  p = pickle.Unpickler(fd)
-  return p.load()
-
-oui = dict()
-
-if not os.path.isfile(ouidat):
-  refreshOUIdata(ouidat)
-
-try:
-  oui = loadOUI(ouidat)
-except:
-  refreshOUIdata(ouidat)
-  oui = loadOUI(ouidat)
-
-if len(sys.argv) == 1:
-  for mac in sys.stdin.readlines():
-    pon(oui,mac)
-else:
-  for mac in sys.argv[1:]:
-    pon(oui,mac)
+    for mac in sys.argv[1:]:
+      print oui.check(mac)

f49a30f3cc529406c4be6a625433349aee7e48b1
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-03-05 16:18:14 +0100

    other/rfc.py - a new improved version

--- other/rfc.py
+++ other/rfc.py
@@ -1,71 +1,86 @@
 #!/usr/bin/env python
 
-# $Id: 20120815$
-# $Date: 2012-08-15 16:43:59$
+# $Id: 20130305$
+# $Date: 2013-03-05 16:16:43$
 # $Author: Marek Lukaszuk$
 
-from subprocess import call,Popen,PIPE
-from time import time
 import os
 import sys
+import time
+import bz2
+import urllib2
+import argparse
 
-rfcdir = "/home/case/store/rfc/"
-maxtime = 3600*24*7;
+class rfc():
 
-wget = "/usr/bin/wget -t 3 -m -q -O "
-bzip2 = "/bin/bzip2 -c"
+  def __init__(self, rfcdir="~/.rfc", indextimeout=7):
 
-if len(sys.argv) < 2:
-  print "usage: "+sys.argv[0]+" (pattern_to_search_for|rfc_number)"
-  sys.exit(1)
+    self.timeout = 3600*24*indextimeout
+    self.wdir = os.path.expanduser(rfcdir)
+    self.idxurl = "https://www.ietf.org/download/rfc-index.txt"
+    self.rfcurl = "https://tools.ietf.org/rfc/"
 
-query = sys.argv[1]
+    if not os.path.exists(self.wdir):
+      os.mkdir(self.wdir,0700)
 
-try:
-  os.mkdir(rfcdir,0700)
-except:
-  pass
+  def fetch(self,query,force=False):
 
-if query.isdigit():
-  try:
+    # this will fetch the given rfc
 
-    if not os.path.isfile(rfcdir+"/rfc"+query+".bz2"):
-      call(wget+" - https://tools.ietf.org/rfc/rfc"+query+".txt | "+bzip2+" > "+rfcdir+"/rfc"+query+".bz2", shell=True)
-    print Popen(bzip2+" -d "+rfcdir+"/rfc"+query+".bz2", shell=True, stdout=PIPE).stdout.read()
-  except:
-    print "can't open rfc"+query
+    if not os.path.isfile(self.wdir+os.sep+"rfc"+query+".bz2") or force:
+      rfc = urllib2.urlopen(self.rfcurl+"rfc"+query+".txt")
+      fd = bz2.BZ2File(self.wdir+os.sep+"rfc"+query+".bz2","wb")
+      fd.write(rfc.read())
+      fd.close()
 
-else:
-  try:
+  def fetchidx(self,force=False):
+
+    # this will fetch the rfc index
+
     try:
-      mtime = (int(time())-int(os.stat(rfcdir+"/rfc-index").st_mtime))/3600
+      mtime = int(time.time()-os.stat(self.wdir+os.sep+"rfc-index.bz2").st_mtime)
     except OSError:
-      mtime = maxtime+1;
+      mtime = self.timeout + 1
 
-    if mtime > maxtime:
-      call(wget+rfcdir+"/rfc-index ftp://ftp.ietf.org/rfc/rfc-index",shell=True)
+    if mtime > self.timeout or force:
+      idx = urllib2.urlopen(self.idxurl)
+      fd = bz2.BZ2File(self.wdir+os.sep+"rfc-index.bz2","wb")
+      fd.write(idx.read())
+      fd.close()
 
-    rfc = open(rfcdir+"/rfc-index","r")
-  except:
-    print "can't open rfc index"
-    sys.exit(1)
+  def query(self,s,force=False):
 
-  title = ""
-  start = False
-  line = 1
+    # query type
 
-  while line:
-    line = rfc.readline()
+    if s.isdigit():
+      # printing given rfc
+      self.fetch(s,force)
+      print bz2.BZ2File(self.wdir+os.sep+"rfc"+s+".bz2").read()
 
-    if line == "\n":
-      if " 0001 " in title:
-        start = True
-
-      if start == True and query.lower() in title:
-        print title
-
-      title = ""
     else:
-      title += " "+line.strip().lower()
+      # search through the index
+      self.fetchidx(force)
+      title = ""
+      for line in bz2.BZ2File(self.wdir+os.sep+"rfc-index.bz2").readlines():
+        if line == "\n":
+          if s.lower() in title:
+            print title
+          title = ""
+        else:
+          title += line.strip().lower()
 
+
+# a helper function
+def query(s,f=False):
+  r = rfc()
+  r.query(s,f)
+
+if __name__ == "__main__":
+
+  p = argparse.ArgumentParser(description='rfc search tool')
+  p.add_argument("query",help="either an RFC number or a string to find in the RFC title")
+  p.add_argument("-f",action='store_true',help="force redownload of either specific RFC or RFC index")
+  args = p.parse_args()
+
+  query(args.query,args.f)
 

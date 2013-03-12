7f29ebc47c3c8f4c721e751903610dfbc25196cf
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-03-12 17:06:49 +0100

    random_fun - small optimizations

--- random_fun/cue/l2.py
+++ random_fun/cue/l2.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 
+# think about:
+# https://en.wikipedia.org/wiki/Sieve_of_Atkin
+import math
+
 primes = [2,3]
 
-def isprime(n):
+def isprime(n,store=True):
 
   global primes
 
   if n == 3 or n == 2 or n == 1:
     return True
 
-  m = (n+1)/2
+  m = int(math.sqrt(n))+1
   for i in primes:
     if n%i == 0:
       return False
     if i > m:
-      break
+      primes.append(n)
+      return True
 
   i = primes[-1]
   while i < m:
@@ -22,56 +27,49 @@ def isprime(n):
       return False
     i+= 2
 
-  primes.append(n)
+  if store:
+    primes.append(n)
   return True
 
 def findfeb():
 
   a = 1
   b = 1
-  c = a
   while True:
-
     c = a + b
     a = b
     b = c
 
     if c > 227000:
-      if isprime(c):
+      if isprime(c,store=False):
         return c
 
 
 dd = dict()
 c = findfeb()+1
-co = c
+m = int(math.sqrt(c))+1
 
-print co
+print str(c-1)
 
-while True:
+while not c == 1:
   d = 2
 
-  while True:
+  if c%d == 0:
+    dd[d] = 1
+    c = c/d
+
+  d = 3
+  while d < m:
 
     if c%d == 0:
       dd[d] = 1
       c = c/d
 
     while True:
-      d += 1
+      d += 2
 
       if isprime(d):
         break
 
-    if d > (co-1)/2:
-      break
-
-  if c == 0 or isprime(c):
-    break
-
 print dd.keys()
-
-s = 0
-for x in dd.keys():
-  s += x
-
-print s
+print sum(list(dd.keys()))

f016179aec4827593ae9b6a923fa8cd3766cb5d9
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-07-22 20:27:24 +0200

    home_stuff/gauth.py - small changes

--- home_stuff/gauth.py
+++ home_stuff/gauth.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python
 
-# $Id: 20121130$
-# $Date: 2012-11-30 07:21:51$
-# $Author: Marek Lukaszuk$
-
-from time import time
+import time
 from struct import pack,unpack
 from hmac import HMAC
 from hashlib import sha1
@@ -18,7 +14,7 @@ except:
   print "Can't read ~/.gauth.conf file"
   exit(1)
 
-tm = int(time() / 30)
+tm = int(time.time() // 30)
 
 lines = fd.read().split("\n")
 for s in lines:
@@ -39,9 +35,7 @@ for s in lines:
   truncatedHash = hm[offset:offset+4]
 
   # get the code from it
-  code = unpack(">L", truncatedHash)[0]
-  code &= 0x7FFFFFFF;
-  code %= 1000000;
+  code = ((unpack(">L", truncatedHash)[0]) & 0x7FFFFFFF ) % 1000000
 
   print str(s[0])+": "+"0"*(6-len(str(code)))+str(code)
   s = fd.read()

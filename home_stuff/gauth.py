9f72d51089f9650475a1b7641c96e89667307c73
Marek Lukaszuk <m.lukaszuk@gmail.com>
2014-02-20 22:02:56 +0100

    home_stuff/gauth.py - small code cleanup

--- home_stuff/gauth.py
+++ home_stuff/gauth.py
@@ -7,34 +7,19 @@ from struct import pack,unpack
 from hmac import HMAC
 from hashlib import sha1
 from base64 import b32decode
-from os import getenv
-from sys import exit
+import os
+import sys
 
-try:
-  fd = open(getenv("HOME")+"/.gauth.conf")
-except:
-  print("Can't read ~/.gauth.conf file")
-  exit(1)
 
-tt = int(time.time())
-tm = tt // 30 # time used for calculations
-td = 30 - (tt % 30)  # reminder of the current time
-
-print("time: ["+(td*"#").ljust(30,".")+"]")
-
-lines = fd.read().split("\n")
-for s in lines:
-  try:
-    s = s.split("|")
-    secretkey = b32decode(s[1].replace(" ","").rstrip().upper())
-  except:
-    continue
-
-  # convert timestamp to raw bytes
-  b = pack(">q", tm)
+def hotp(key,counter):
+  """
+  http://tools.ietf.org/html/rfc4226
+  """
+  # convert counter to raw bytes
+  b = pack(">q", counter)
 
   # generate HMAC-SHA1 from timestamp based on secret key
-  hm = HMAC(secretkey, b, sha1).digest()
+  hm = HMAC(key, b, sha1).digest()
 
   # extract 4 bytes from digest based on LSB
   offset = ord(hm[-1]) & 0x0F
@@ -43,5 +28,34 @@ for s in lines:
   # get the code from it
   code = ((unpack(">L", truncatedHash)[0]) & 0x7FFFFFFF ) % 1000000
 
-  print(str(s[0])+": "+"0"*(6-len(str(code)))+str(code))
-  s = fd.read()
+  return "0"*(6-len(str(code)))+str(code)
+
+def totp(key):
+  """
+  http://tools.ietf.org/html/rfc6238
+  """
+  return hotp(key,int(time.time())//30)
+
+if __name__ == "__main__":
+
+  try:
+    fd = open(os.getenv("HOME")+"/.gauth.conf")
+  except:
+    print("Can't read ~/.gauth.conf file")
+    sys.exit(1)
+
+  td = 30 - (int(time.time()) % 30)  # reminder of the current time
+
+  print("time: ["+(td*"#").ljust(30,".")+"]")
+
+  lines = fd.read().split("\n")
+  for s in lines:
+    try:
+      s = s.split("|")
+      secretkey = b32decode(s[1].replace(" ","").rstrip().upper())
+    except:
+      continue
+
+    print(str(s[0]).ljust(30,".")+": "+totp(secretkey))
+    s = fd.read()
+  fd.close()

53535738c972afeed5e1a9a41b3f9f3d6f530480
Marek Lukaszuk <m.lukaszuk@gmail.com>
2014-09-30 10:09:40 +0200

    gpg_refresh_keys.py - fxied broken sleep delay ;)

--- other/gpg_refresh_keys.py
+++ other/gpg_refresh_keys.py
@@ -20,12 +20,12 @@ GPGUPDATEKEY = GPGBIN + " --batch -q "+\
     "--keyserver-options no-honor-keyserver-url,http-proxy=%s " % (HTTP_PROXY)+\
     "--recv-keys "
 
-def maxwait(limit, keys):
+def maxwait(endtime, keys):
   # how long we can sleep between each key check
   # given that this should be random we should in the long run land somewhere
   # around the half of this number, this is why it is multiplied by 1.5
   if len(keys) > 0:
-    max_wait = int((limit*3600)//len(keys) * 1.5)
+    max_wait = (endtime - int(time.time()))//len(keys) * 1.5)
 
     # make sure we have a safe minimum limit
     if max_wait < 15:
@@ -45,42 +45,47 @@ class Unbuffered:
   def __getattr__(self, attr):
     return getattr(self.stream, attr)
 
-sys.stdout = Unbuffered(sys.stdout)
+if __name__ == "__main__":
 
-print("starting the refresh process")
-print("GPGUPDATEKEY: %s" % (GPGUPDATEKEY))
-print("GPGLISTKEYS: %s" % (GPGLISTKEYS))
+  sys.stdout = Unbuffered(sys.stdout)
 
-# building a list of the keys that we can update
-keys_to_check = list()
-for line in subprocess.check_output(shlex.split(GPGLISTKEYS)).split("\n"):
-  if line.startswith("pub:-:"):
-    keys_to_check.append((line.split(':')[4]))
+  print("starting the refresh process")
+  print("GPGUPDATEKEY: %s" % (GPGUPDATEKEY))
+  print("GPGLISTKEYS: %s" % (GPGLISTKEYS))
 
-print("collected the keys to refresh: %s" % (len(keys_to_check)))
+  # building a list of the keys that we can update
+  keys_to_check = list()
+  for line in subprocess.check_output(shlex.split(GPGLISTKEYS)).split("\n"):
+    if line.startswith("pub:-:"):
+      keys_to_check.append((line.split(':')[4]))
 
-max_wait = maxwait(REFRESH_LIMIT_HOURS, keys_to_check)
-# random sleep at begining
-time.sleep(random.randint(1, max_wait))
+  # we should finish in the worst case at this time
+  ENDTIME=int(time.time()) + (REFRESH_LIMIT_HOURS * 3600)
+  max_wait = maxwait(ENDTIME, keys_to_check)
+  print("collected the keys to refresh: %s, max_wait: %s" % \
+      (len(keys_to_check),max_wait))
 
-while keys_to_check:
+  # random sleep at begining
+  time.sleep(random.randint(1, max_wait))
 
-  print("keys left to process: %s, current max_wait %s " % \
-      (len(keys_to_check), max_wait))
+  while keys_to_check:
 
-  # pick random key to check
-  keyid = random.choice(keys_to_check)
+    print("keys left to process: %s, current max_wait %s " % \
+        (len(keys_to_check), max_wait))
 
-  # see if there is an update
-  try:
-    subprocess.check_call(shlex.split(GPGUPDATEKEY+keyid))
-  except:
-    # if there was a problem refreshing this key we will try
-    # again on another round, not this one
-    pass
+    # pick random key to check
+    keyid = random.choice(keys_to_check)
 
-  keys_to_check.remove(keyid)
-  max_wait = maxwait(REFRESH_LIMIT_HOURS, keys_to_check)
+    # see if there is an update
+    try:
+      subprocess.check_call(shlex.split(GPGUPDATEKEY+keyid))
+    except:
+      # if there was a problem refreshing this key we will try
+      # again on another round, not this one
+      pass
 
-  # random sleep between key checks
-  time.sleep(random.randint(1,max_wait))
+    keys_to_check.remove(keyid)
+    max_wait = maxwait(ENDTIME, keys_to_check)
+
+    # random sleep between key checks
+    time.sleep(random.randint(1,max_wait))

cc5763a95f9f62c6a540b194cc7e6256a5ed4132
Marek Lukaszuk <m.lukaszuk@gmail.com>
2015-01-13 18:07:26 +0100

    finding_outif_forIPv4.py - finding out if based on given IPv4 address

---
+++ scapy/finding_outif_forIPv4.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+from scapy.all import *
+
+"""
+usage: ./scriptname.py <ipv4 address to test>
+"""
+
+def find_out_if(ipv4):
+    ipv4 = int("".join([chr(int(i)) for i in ipv4.split('.')]).encode('hex'),16)
+    out = ""
+    for route in sorted(read_routes(), key=lambda x: x[1], reverse=True):
+        if ipv4 & route[1] == route[0]:
+            out = "if: %s" % (route[3])
+            break
+    return out
+
+print find_out_if(sys.argv[1]) 

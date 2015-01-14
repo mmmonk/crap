4e909dea555919e33d7e9bdeb82af35719dfcf76
Marek Lukaszuk <m.lukaszuk@gmail.com>
2015-01-14 11:01:35 +0100

    finding_outif_forIPv4.py - removed usless parts

--- scapy/finding_outif_forIPv4.py
+++ scapy/finding_outif_forIPv4.py
@@ -8,10 +8,9 @@ usage: ./scriptname.py <ipv4 address to test>
 """
 
 def find_out_if(ipv4):
-    ipv4 = int("".join([chr(int(i)) for i in ipv4.split('.')]).encode('hex'),16)
-    for route in sorted(read_routes(), key=lambda x: x[1], reverse=True):
-        if ipv4 & route[1] == route[0]:
-            return route[3]
-    return ""
+  ipv4 = int("".join([chr(int(i)) for i in ipv4.split('.')]).encode('hex'),16)
+  for route in sorted(read_routes(), key=lambda x: x[1], reverse=True):
+    if ipv4 & route[1] == route[0]: return route[3]
+  return "?"
 
 print find_out_if(sys.argv[1]) 

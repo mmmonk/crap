a7b05fb67f776558a73f3ef7a6cd872105c6f067
Marek Lukaszuk <m.lukaszuk@gmail.com>
2015-01-18 15:14:57 +0100

    gg_server_tester.py - change behaviour

--- home_stuff/gg_server_tester.py
+++ home_stuff/gg_server_tester.py
@@ -7,8 +7,7 @@ from random import choice as rndchoice
 from os import getenv
 
 servers = []
-
-socket.setdefaulttimeout(5)
+socket.setdefaulttimeout(3)
 
 def testserver(srv):
   s = socket.socket()
@@ -19,12 +18,19 @@ def testserver(srv):
     s.close()
     conn_time = time()-start_time
     if str(data).encode('hex') == "0100":
-      if conn_time > 0.04:
-        return False
-      return True
-    return False
+      return conn_time 
+    return 0 
   except socket.error:
-    return False
+    return 0
+
+def useserver(srv):
+  try:
+    open(str(getenv('HOME'))+"/.gg/cmd","w").write(
+      "/set server %s\n" % (srv) +\
+      "/wr\n/reconnect\n/beep\n")
+    exit(0)
+  except:
+    exit(1)
 
 if __name__ == '__main__':
 
@@ -32,6 +38,7 @@ if __name__ == '__main__':
   for o4 in xrange(2,100):
     servers.append("91.214.237."+str(o4))
 
+  alive = {}
   # test servers
   while len(servers) > 0:
 
@@ -39,14 +46,13 @@ if __name__ == '__main__':
     server = rndchoice(servers)
     servers.remove(server)
 
-    if testserver(server):
-      print server
-      try:
-        open(str(getenv('HOME'))+"/.gg/cmd","w").write("\
-            /set server "+str(server)+"\n\
-            /wr\n\
-            /reconnect\n\
-            /beep\n")
-      except:
-        continue
-      exit(0)
+    result = testserver(server)
+    if result > 0:
+      alive[server] = result
+      if result <= 0.05:
+        print server
+        useserver(server)
+
+  tmpsrv = sorted(alive.iteritems(), key=lambda srv: srv[1])
+  alive = [item[0] for item in tmpsrv]
+  useserver(rndchoice(alive))

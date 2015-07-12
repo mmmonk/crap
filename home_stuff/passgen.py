1d665c2360c6f0dfba06f93f0e281672615f36d9
Marek Lukaszuk <m.lukaszuk@gmail.com>
2015-07-12 19:18:06 +0200

    passgen.py - added some stuff

--- home_stuff/passgen.py
+++ home_stuff/passgen.py
@@ -2,6 +2,12 @@
 
 # generate passwords based on the URI+somerandom stuff
 
+# TODO:
+# - add a choice of possible strings characters, although if the site doesn't
+#   allow special characteres they have some other issues,
+# - maybe add a limit on the lenght of the password,
+# - maybe SHA256 is good enough, and there is no need for SHA512?
+
 import hashlib
 import string
 import argparse
@@ -16,8 +22,9 @@ def passgen(uri, secret, passchars, halgo="sha512", iterations=10000):
 
 if __name__ == "__main__":
 
-  p = argparse.ArgumentParser(description='Simple password generator')
-  p.add_argument('-uri', '-u', required=True, help="URI")
+  p = argparse.ArgumentParser(description='Simple recoverable passwords '+\
+      'generator - based on URI and a secret')
+  p.add_argument('-uri', '-u', required=True, help="URI, proto://user@fqdn")
   p.add_argument('-secret', '-s', required=True, help="secret")
   args = p.parse_args()
 

6606a47e93d32c53fd2c7babbcee4454a070d68d
Marek Lukaszuk <m.lukaszuk@gmail.com>
2016-10-03 22:34:30 +0200

    stocks_monitor.py - added some caching

--- other/stocks_monitor.py
+++ other/stocks_monitor.py
@@ -3,12 +3,21 @@
 import urllib
 import json
 import sys
+import os
+import time
 
 if len(sys.argv) > 1:
   symbol = str(sys.argv[1])
-  a = urllib.urlopen("http://www.google.com/finance/info?q=%s" % (symbol))
-  b = json.loads(a.read().replace("\n","")[2:])
-  out = "%s:" % (symbol)
-  for a in b:
-    out += "%s " % (a['l'])
+  uid = os.geteuid()
+  path = "/run/user/%s/stock_%s.txt" % (uid, symbol)
+  out = ""
+  if os.path.exists(path) and time.time() - os.stat(path).st_mtime > 600:
+    out = open(path).read()
+  else:
+    a = urllib.urlopen("http://www.google.com/finance/info?q=%s" % (symbol))
+    b = json.loads(a.read().replace("\n","")[2:])
+    out = "%s:" % (symbol)
+    for a in b:
+      out += "%s " % (a['l'])
+    open(path,"w").write(out)
   print out
